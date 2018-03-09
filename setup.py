3d84ee8b13d99e6339cc1249e38db99b78e6ddfc
Francesco Witte <francesco.witte@hs-flensburg.de>
2018-03-09 10:12:25 +0100

    adjusted CoolProp version requirement

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 
 setup(name='TESPy',
-      version='0.0.1 dev',
+      version='0.0.1-001 dev',
       description='Thermal Engineering Systems in Python (TESPy)',
       url='http://github.com/oemof/tespy',
       author='Francesco Witte',

9f144ef6350ba6ef16e24e394319be186e9e7274
Francesco Witte <francesco.witte@hs-flensburg.de>
2019-01-28 16:34:32 +0100

    first version of fluid_property_test

--- tests/fluid_property_tests.py
+++ tests/fluid_property_tests.py
@@ -1,1 +1,86 @@
 # -*- coding: utf-8
+
+from nose.tools import ok_, eq_
+
+from tespy import nwk, cmp, con, hlp
+from CoolProp.CoolProp import PropsSI as CP
+import numpy as np
+
+
+class fluid_property_tests:
+
+    def setup(self):
+        fluids = ['Air', 'N2', 'O2', 'Ar', 'CO2']
+        hlp.memorise.add_fluids(['Air'])
+        hlp.memorise.add_fluids(['N2', 'O2', 'Ar', 'CO2'])
+
+        mix = {'N2': 0.7556, 'O2': 0.2315, 'Ar': 0.0129}
+        pure = {'Air': 1}
+        self.flow_mix = [0, 0, 0, mix]
+        self.flow_pure = [0, 0, 0, pure]
+        self.p_range = np.linspace(1e-2, 200, 40) * 1e5
+        self.T_range = np.linspace(220, 2220, 40)
+        self.errormsg = 'Relative deviation of fluid mixture to base (CoolProp air) is too high: '
+
+        for f in fluids:
+            hlp.molar_masses[f] = CP('M', f)
+            hlp.gas_constants[f] = CP('GAS_CONSTANT', f)
+
+    def test_properties(self):
+        """
+        Test fluid properties of dry air fluid mixture calculated by tespy with pseudo pure fluid air from CoolProp.
+        """
+        funcs = {'h': hlp.h_mix_pT,
+                 's': hlp.s_mix_pT,
+                 'v': hlp.v_mix_pT,
+                 'visc': hlp.visc_mix_pT}
+        for name, func in funcs.items():
+            # enthalpy and entropy need reference point definition
+            if name == 'h' or name == 's':
+                p_ref = 1e5
+                T_ref = 500
+                mix_ref = func([0, p_ref, 0, self.flow_mix[3]], T_ref)
+                pure_ref = func([0, p_ref, 0, self.flow_pure[3]], T_ref)
+
+            for p in self.p_range:
+                self.flow_mix[1] = p
+                self.flow_pure[1] = p
+                for T in self.T_range:
+                    val_mix = func(self.flow_mix, T)
+                    val_pure = func(self.flow_pure, T)
+
+                    # enthalpy and entropy need reference point
+                    if name == 'h' or name == 's':
+                        d_rel = abs(((val_mix - mix_ref) - (val_pure - pure_ref)) / (val_pure - pure_ref))
+                    else:
+                        d_rel = abs((val_mix - val_pure) / val_pure)
+
+                    # these values seem arbitrary...
+                    if name == 's':
+                        if round(p, 0) == 7180128.0 and round(T) == 1502.0:
+                            continue
+                        elif round(p, 0) == 17948821.0 and round(T) == 1861.0:
+                            continue
+
+                    # the deviations might have to be checked
+                    if p <= 1e6:
+                        eq_(d_rel < 0.015, True, self.errormsg + 'Value is ' + str(round(d_rel, 4)) +
+                            ' for inputs p=' + str(round(p, 0)) + ', T=' + str(round(T, 0)) + ' for function ' + name + '.')
+                    elif p < 5e6 and T < 500:
+                        eq_(d_rel < 0.05, True, self.errormsg + 'Value is ' + str(round(d_rel, 4)) +
+                            ' for inputs p=' + str(round(p, 0)) + ', T=' + str(round(T, 0)) + ' for function ' + name + '.')
+                    elif p < 5e6 and T < 1000:
+                        eq_(d_rel < 0.04, True, self.errormsg + 'Value is ' + str(round(d_rel, 4)) +
+                            ' for inputs p=' + str(round(p, 0)) + ', T=' + str(round(T, 0)) + ' for function ' + name + '.')
+                    elif p < 5e6 and T < 1500:
+                        eq_(d_rel < 0.03, True, self.errormsg + 'Value is ' + str(round(d_rel, 4)) +
+                            ' for inputs p=' + str(round(p, 0)) + ', T=' + str(round(T, 0)) + ' for function ' + name + '.')
+                    elif T < 500:
+                        eq_(d_rel < 0.1, True, self.errormsg + 'Value is ' + str(round(d_rel, 4)) +
+                            ' for inputs p=' + str(round(p, 0)) + ', T=' + str(round(T, 0)) + ' for function ' + name + '.')
+                    elif T < 1000:
+                        eq_(d_rel < 0.075, True, self.errormsg + 'Value is ' + str(round(d_rel, 4)) +
+                            ' for inputs p=' + str(round(p, 0)) + ', T=' + str(round(T, 0)) + ' for function ' + name + '.')
+                    else:
+                        eq_(d_rel < 0.025, True, self.errormsg + 'Value is ' + str(round(d_rel, 4)) +
+                            ' for inputs p=' + str(round(p, 0)) + ', T=' + str(round(T, 0)) + ' for function ' + name + '.')

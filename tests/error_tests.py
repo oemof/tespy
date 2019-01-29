e575446df62f9b5f74d9713506d1eab34b74c1c6
Francesco Witte <francesco.witte@hs-flensburg.de>
2019-01-29 10:17:31 +0100

    fixed tests

--- tests/error_tests.py
+++ tests/error_tests.py
@@ -7,6 +7,9 @@ from CoolProp.CoolProp import PropsSI as CP
 import numpy as np
 
 
+# %% bulk tests
+
+
 class specification_error_tests:
 
     def setup(self):
@@ -16,21 +19,35 @@ class specification_error_tests:
         self.bus = con.bus('mybus')
 
     @raises(ValueError)
-    def test_cmp_instanciation(self):
-        labels = [5, 'Label,', 'Labe;l', 'Label.']
-        for l in labels:
-            cmp.cogeneration_unit(l)
-
-        # interface specification
-        cmp.sink('sink', interface=5)
+    def cmp_instanciation_ValueError(self, label):
+        cmp.cogeneration_unit(label)
 
     @raises(ValueError)
     def set_attr_ValueError(self, instance, **kwargs):
         instance.set_attr(**kwargs)
 
+    @raises(ValueError)
+    def create_connection_ValueError(self, pos):
+        if pos == 'source':
+            con.connection(self.comp, 'out6', self.pipe, 'in1')
+        elif pos == 'target':
+            con.connection(self.comp, 'out1', self.pipe, 'in5')
+
     @raises(TypeError)
     def set_attr_TypeError(self, instance, **kwargs):
         instance.set_attr(**kwargs)
+
+    @raises(TypeError)
+    def bus_add_comps_TypeError(self, c):
+        self.bus.add_comps(c)
+
+    @raises(TypeError)
+    def test_create_conn_TypeError(self):
+        con.connection(self.comp, 'out1', self.conn, 'in1')
+
+    @raises(TypeError)
+    def create_ref_TypeError(self, params):
+        con.ref(params[0], params[1], params[2])
 
     @raises(KeyError)
     def set_attr_KeyError(self, instance, **kwargs):
@@ -53,14 +70,18 @@ class specification_error_tests:
         self.bus.add_comps(self.comp)
 
     def test_set_attr_errors(self):
+        #
+        labels = [5, 'Label,', 'Labe;l', 'Label.']
+        for l in labels:
+            self.cmp_instanciation_ValueError(l)
+
         # ValueErrors
         self.set_attr_ValueError(self.comp, mode=5)
         self.set_attr_ValueError(self.comp, offdesign=['Q'])
         self.set_attr_ValueError(self.conn, offdesign=['f'])
 
-
-#        con.connection(self.comp, 'out1', self.pipe, 'in5')
-#        con.connection(self.comp, 'out6', self.pipe, 'in1')
+        self.create_connection_ValueError('source')
+        self.create_connection_ValueError('target')
 
         # TypeErrors
         self.set_attr_TypeError(self.comp, P=[5])
@@ -71,15 +92,15 @@ class specification_error_tests:
         self.set_attr_TypeError(self.conn, h0=[4])
         self.set_attr_TypeError(self.conn, fluid=5)
 
-#        con.connection(self.comp, 'out1', self.conn, 'in5')
-#        self.bus.add_comps({'c': self.conn})
-#        self.bus.add_comps({'f': self.comp})
-#        self.bus.add_comps({'c': self.comp, 'char': 'Hi'})
-#        self.bus.add_comps({'c': self.comp, 'p': 5})
-#        self.bus.add_comps({'c': self.comp, 'P_ref': 'what'})
-#        con.connection(self.comp, 'out1', self.pipe, 'in1', m=con.ref(self.conn, 7, 'hi'))
-#        con.connection(self.comp, 'out1', self.pipe, 'in1', m=con.ref(self.conn, 'Hi', 0))
-#        con.connection(self.comp, 'out1', self.pipe, 'in1', m=con.ref(self.comp, 1, 0))
+        self.bus_add_comps_TypeError({'c': self.conn})
+        self.bus_add_comps_TypeError({'f': self.comp})
+        self.bus_add_comps_TypeError({'c': self.comp, 'char': 'Hi'})
+        self.bus_add_comps_TypeError({'c': self.comp, 'p': 5})
+        self.bus_add_comps_TypeError({'c': self.comp, 'P_ref': 'what'})
+
+        self.create_ref_TypeError([self.conn, 7, 'hi'])
+        self.create_ref_TypeError([self.conn, 'hi', 0])
+        self.create_ref_TypeError([self.cimp, 1, 0])
 
         # KeyErrors
         self.set_attr_KeyError(self.comp, wow=5)
@@ -89,3 +110,10 @@ class specification_error_tests:
         self.get_attr_KeyError(self.conn, 'key')
         self.get_attr_KeyError(self.bus, 'components')
         self.get_attr_KeyError(con.ref(self.conn, 1, 0), 'comp')
+
+# %% Single tests
+
+@raises(ValueError)
+def test_interface_ValueError():
+    # interface specification
+    cmp.sink('sink', interface=5)

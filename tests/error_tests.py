721e11ca0390510e0fb1f5b31774158797ac02d9
Francesco Witte <francesco.witte@hs-flensburg.de>
2019-01-29 09:56:04 +0100

    adjustments to tests

--- tests/error_tests.py
+++ tests/error_tests.py
@@ -25,42 +25,67 @@ class specification_error_tests:
         cmp.sink('sink', interface=5)
 
     @raises(ValueError)
-    def test_set_attr_ValueError(self):
-        self.comp.set_attr(mode=5)
-        self.comp.set_attr(offdesign=['Q'])
-        con.connection(self.comp, 'out1', self.pipe, 'in5')
-        con.connection(self.comp, 'out6', self.pipe, 'in1')
-        self.conn.set_attr(offdesign=['f'])
+    def set_attr_ValueError(self, instance, **kwargs):
+        instance.set_attr(**kwargs)
 
     @raises(TypeError)
-    def test_set_attr_TypeError(self):
-        self.comp.set_attr(P=[5], tiP_char=None)
-        con.connection(self.comp, 'out1', self.conn, 'in5')
-        self.conn.set_attr(design='h', fluid_balance=1, fluid=5, h0=[4])
-        self.bus.add_comps({'c': self.conn})
-        self.bus.add_comps({'f': self.comp})
-        self.bus.add_comps({'c': self.comp, 'char': 'Hi'})
-        self.bus.add_comps({'c': self.comp, 'p': 5})
-        self.bus.add_comps({'c': self.comp, 'P_ref': 'what'})
-        con.connection(self.comp, 'out1', self.pipe, 'in1', m=con.ref(self.conn, 7, 'hi'))
-        con.connection(self.comp, 'out1', self.pipe, 'in1', m=con.ref(self.conn, 'Hi', 0))
-        con.connection(self.comp, 'out1', self.pipe, 'in1', m=con.ref(self.comp, 1, 0))
+    def set_attr_TypeError(self, instance, **kwargs):
+        instance.set_attr(**kwargs)
 
     @raises(KeyError)
-    def test_set_attr_KeyError(self):
-        self.comp.set_attr(wow=5)
-        self.conn.set_attr(key=7)
-        self.comp.get_attr('wow')
-        self.conn.get_attr('key')
-        self.bus.get_attr('components')
-        con.ref(self.conn, 1, 0).get_attr('comp')
+    def set_attr_KeyError(self, instance, **kwargs):
+        instance.set_attr(**kwargs)
+
+    @raises(KeyError)
+    def set_attr_KeyError(self, instance, key):
+        instance.get_attr(key)
 
     @raises(hlp.TESPyComponentError)
-    def test_set_attr_TESPyError(self):
+    def test_TESPyComponentError(self):
         self.comp.set_attr(interface=True)
 
     @raises(hlp.TESPyConnectionError)
-    def test_con_instanciation(self):
+    def test_con_TESPyConnectionError(self):
         con.connection(self.comp, 'out1', self.comp, 'in1')
+
+    @raises(hlp.TESPyConnectionError)
+    def test_bus_TESPyConnectionError(self):
         self.bus.add_comps(self.comp)
 
+    def test_set_attr_errors(self):
+        # ValueErrors
+        self.set_attr_ValueError(self.comp, mode=5)
+        self.set_attr_ValueError(self.comp, offdesign=['Q'])
+        self.set_attr_ValueError(self.conn, offdesign=['f'])
+
+
+#        con.connection(self.comp, 'out1', self.pipe, 'in5')
+#        con.connection(self.comp, 'out6', self.pipe, 'in1')
+
+        # TypeErrors
+        self.set_attr_TypeError(self.comp, P=[5])
+        self.set_attr_TypeError(self.comp, tiP_char=None)
+
+        self.set_attr_TypeError(self.conn, design='h')
+        self.set_attr_TypeError(self.conn, fluid_balance=1)
+        self.set_attr_TypeError(self.conn, h0=[4])
+        self.set_attr_TypeError(self.conn, fluid=5)
+
+#        con.connection(self.comp, 'out1', self.conn, 'in5')
+#        self.bus.add_comps({'c': self.conn})
+#        self.bus.add_comps({'f': self.comp})
+#        self.bus.add_comps({'c': self.comp, 'char': 'Hi'})
+#        self.bus.add_comps({'c': self.comp, 'p': 5})
+#        self.bus.add_comps({'c': self.comp, 'P_ref': 'what'})
+#        con.connection(self.comp, 'out1', self.pipe, 'in1', m=con.ref(self.conn, 7, 'hi'))
+#        con.connection(self.comp, 'out1', self.pipe, 'in1', m=con.ref(self.conn, 'Hi', 0))
+#        con.connection(self.comp, 'out1', self.pipe, 'in1', m=con.ref(self.comp, 1, 0))
+
+        # KeyErrors
+        self.set_attr_KeyError(self.comp, wow=5)
+        self.set_attr_KeyError(self.conn, jey=5)
+
+        self.get_attr_KeyError(self.comp, 'wow')
+        self.get_attr_KeyError(self.conn, 'key')
+        self.get_attr_KeyError(self.bus, 'components')
+        self.get_attr_KeyError(con.ref(self.conn, 1, 0), 'comp')

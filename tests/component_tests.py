9b1f4734ff76d7791771a1b1b6d4f7275e89a9d2
Francesco Witte <francesco.witte@hs-flensburg.de>
2019-01-31 18:45:22 +0100

    added tests for combustion chamber, cogeneration unit and valve

--- tests/component_tests.py
+++ tests/component_tests.py
@@ -12,7 +12,7 @@ import shutil
 class component_tests:
 
     def setup(self):
-        self.nw = nwk.network(['INCOMP::DowQ', 'H2O', 'NH3', 'N2', 'O2', 'Ar'],
+        self.nw = nwk.network(['INCOMP::DowQ', 'H2O', 'NH3', 'N2', 'O2', 'Ar', 'CO2', 'CH4'],
                               T_unit='C', p_unit='bar', v_unit='m3 / s')
         self.source = cmp.source('source')
         self.sink = cmp.sink('sink')
@@ -26,13 +26,23 @@ class component_tests:
         self.nw.add_conns(c1, c2)
         return c1, c2
 
+    def setup_network_21(self, instance):
+        """
+        Set up network for components with two inlets and one outlet.
+        """
+        c1 = con.connection(self.source, 'out1', instance, 'in1')
+        c2 = con.connection(cmp.source('fuel'), 'out1', instance, 'in2')
+        c3 = con.connection(instance, 'out1', self.sink, 'in1')
+        self.nw.add_conns(c1, c2, c3)
+        return c1, c2, c3
+
     def test_turbomachine(self):
         """
         Test component properties of turbomachines.
         """
         instance = cmp.turbomachine('turbomachine')
         c1, c2 = self.setup_network_11(instance)
-        fl = {'N2': 0.7556, 'O2': 0.2315, 'Ar': 0.0129, 'INCOMP::DowQ': 0, 'H2O': 0, 'NH3': 0}
+        fl = {'N2': 0.7556, 'O2': 0.2315, 'Ar': 0.0129, 'INCOMP::DowQ': 0, 'H2O': 0, 'NH3': 0, 'CO2': 0, 'CH4': 0}
         c1.set_attr(fluid=fl, m=10, p=1, h=1e5)
         c2.set_attr(p=1, h=2e5)
         self.nw.solve('design')
@@ -68,7 +78,7 @@ class component_tests:
         """
         instance = cmp.pump('pump')
         c1, c2 = self.setup_network_11(instance)
-        fl = {'N2': 0, 'O2': 0, 'Ar': 0, 'INCOMP::DowQ': 1, 'H2O': 0, 'NH3': 0}
+        fl = {'N2': 0, 'O2': 0, 'Ar': 0, 'INCOMP::DowQ': 1, 'H2O': 0, 'NH3': 0, 'CO2': 0, 'CH4': 0}
         c1.set_attr(fluid=fl, v=1, p=5,T=50)
         c2.set_attr(p=7)
         instance.set_attr(eta_s=1)
@@ -112,7 +122,7 @@ class component_tests:
         """
         instance = cmp.compressor('compressor')
         c1, c2 = self.setup_network_11(instance)
-        fl = {'N2': 0, 'O2': 0, 'Ar': 0, 'INCOMP::DowQ': 0, 'H2O': 0, 'NH3': 1}
+        fl = {'N2': 0, 'O2': 0, 'Ar': 0, 'INCOMP::DowQ': 0, 'H2O': 0, 'NH3': 1, 'CO2': 0, 'CH4': 0}
         c1.set_attr(fluid=fl, v=1, p=5, T=100)
         c2.set_attr(p=7)
         instance.set_attr(eta_s=0.8)
@@ -164,7 +174,7 @@ class component_tests:
         """
         instance = cmp.turbine('turbine')
         c1, c2 = self.setup_network_11(instance)
-        fl = {'N2': 0.7556, 'O2': 0.2315, 'Ar': 0.0129, 'INCOMP::DowQ': 0, 'H2O': 0, 'NH3': 0}
+        fl = {'N2': 0.7556, 'O2': 0.2315, 'Ar': 0.0129, 'INCOMP::DowQ': 0, 'H2O': 0, 'NH3': 0, 'CO2': 0, 'CH4': 0}
         c1.set_attr(fluid=fl, m=15, p=10, T=120)
         c2.set_attr(p=1)
         instance.set_attr(eta_s=0.8)
@@ -205,3 +215,116 @@ class component_tests:
         except ValueError:
             pass
         shutil.rmtree('./tmp', ignore_errors=True)
+
+    def test_combustion_chamber(self):
+        """
+        Test component properties of combustion chambers.
+        """
+        instance = cmp.combustion_chamber('combustion chamber', fuel='CH4')
+        c1, c2, c3 = self.setup_network_21(instance)
+        air = {'N2': 0.7556, 'O2': 0.2315, 'Ar': 0.0129, 'INCOMP::DowQ': 0, 'H2O': 0, 'NH3': 0, 'CO2': 0, 'CH4': 0}
+        fuel = {'N2': 0, 'O2': 0, 'Ar': 0, 'INCOMP::DowQ': 0, 'H2O': 0, 'NH3': 0, 'CO2': 0.04, 'CH4': 0.96}
+        c1.set_attr(fluid=air, p=1, T=30)
+        c2.set_attr(fluid=fuel, T=30)
+        c3.set_attr(T=1200)
+        b = con.bus('thermal input', P=1e6)
+        b.add_comps({'c': instance})
+        self.nw.add_busses(b)
+        self.nw.solve('design')
+        self.nw.print_results()
+        eq_(round(b.P.val, 1), round(instance.ti.val, 1), 'Value of thermal input must be ' + str(b.P.val) + ', is ' + str(instance.ti.val) + '.')
+
+    def test_valve(self):
+        """
+        Test component properties of valves.
+        """
+        instance = cmp.valve('valve')
+        c1, c2 = self.setup_network_11(instance)
+        fl = {'N2': 0.7556, 'O2': 0.2315, 'Ar': 0.0129, 'INCOMP::DowQ': 0, 'H2O': 0, 'NH3': 0, 'CO2': 0, 'CH4': 0}
+        c1.set_attr(fluid=fl, m=10, p=10, T=120)
+        c2.set_attr(p=1)
+        instance.set_attr(pr='var')
+        self.nw.solve('design')
+        eq_(round(c2.p.val_SI / c1.p.val_SI, 1), round(instance.pr.val, 1), 'Value of pressure ratio must be ' + str(c2.p.val_SI / c1.p.val_SI) + ', is ' + str(instance.pr.val) + '.')
+        zeta = instance.zeta.val
+        instance.set_attr(zeta='var', pr=np.nan)
+        self.nw.solve('design')
+        eq_(round(zeta, 1), round(instance.zeta.val, 1), 'Value of pressure ratio must be ' + str(zeta) + ', is ' + str(instance.zeta.val) + '.')
+
+    def test_cogeneration_unit(self):
+        """
+        Test component properties of cogeneration unit.
+        """
+        amb = cmp.source('ambient')
+        sf = cmp.source('fuel')
+        fg = cmp.sink('flue gas outlet')
+        cw_in1 = cmp.source('cooling water inlet1')
+        cw_in2 = cmp.source('cooling water inlet2')
+        cw_out1 = cmp.sink('cooling water outlet1')
+        cw_out2 = cmp.sink('cooling water outlet2')
+        chp = cmp.cogeneration_unit('cogeneration unit', fuel='CH4')
+        amb_comb = con.connection(amb, 'out1', chp, 'in3')
+        sf_comb = con.connection(sf, 'out1', chp, 'in4')
+        comb_fg = con.connection(chp, 'out3', fg, 'in1')
+        self.nw.add_conns(sf_comb, amb_comb, comb_fg)
+        cw1_chp1 = con.connection(cw_in1, 'out1', chp, 'in1')
+        cw2_chp2 = con.connection(cw_in2, 'out1', chp, 'in2')
+        self.nw.add_conns(cw1_chp1, cw2_chp2)
+        chp1_cw = con.connection(chp, 'out1', cw_out1, 'in1')
+        chp2_cw = con.connection(chp, 'out2', cw_out2, 'in1')
+        self.nw.add_conns(chp1_cw, chp2_cw)
+
+        air = {'N2': 0.7556, 'O2': 0.2315, 'Ar': 0.0129, 'INCOMP::DowQ': 0, 'H2O': 0, 'NH3': 0, 'CO2': 0, 'CH4': 0}
+        fuel = {'N2': 0, 'O2': 0, 'Ar': 0, 'INCOMP::DowQ': 0, 'H2O': 0, 'NH3': 0, 'CO2': 0.04, 'CH4': 0.96}
+        water1 = {'N2': 0, 'O2': 0, 'Ar': 0, 'INCOMP::DowQ': 0, 'H2O': 1, 'NH3': 0, 'CO2': 0, 'CH4': 0}
+        water2 = {'N2': 0, 'O2': 0, 'Ar': 0, 'INCOMP::DowQ': 0, 'H2O': 1, 'NH3': 0, 'CO2': 0, 'CH4': 0}
+
+        chp.set_attr(fuel='CH4', pr1=0.99, pr2=0.99, lamb=1.2, design=['pr1', 'pr2'], offdesign=['zeta1', 'zeta2'])
+        amb_comb.set_attr(p=5, T=30, fluid=air)
+        sf_comb.set_attr(T=30, fluid=fuel)
+        cw1_chp1.set_attr(p=3, T=60, m=50, fluid=water1)
+        cw2_chp2.set_attr(p=3, T=80, m=50, fluid=water2)
+
+        TI = con.bus('thermal input')
+        Q1 = con.bus('heat output 1')
+        Q2 = con.bus('heat output 2')
+        Q = con.bus('heat output')
+        Qloss = con.bus('thermal heat loss')
+
+        TI.add_comps({'c': chp, 'p': 'TI'})
+        Q1.add_comps({'c': chp, 'p': 'Q1'})
+        Q2.add_comps({'c': chp, 'p': 'Q2'})
+        Q.add_comps({'c': chp, 'p': 'Q'})
+        Qloss.add_comps({'c': chp, 'p': 'Qloss'})
+
+        self.nw.add_busses(TI, Q1, Q2, Q, Qloss)
+        ti = 1e6
+        TI.set_attr(P=ti)
+
+        self.nw.solve('design')
+        self.nw.save('tmp')
+        self.nw.solve('offdesign', init_path='tmp', design_path='tmp')
+        eq_(round(TI.P.val, 1), round(chp.ti.val, 1), 'Value of thermal input must be ' + str(TI.P.val) + ', is ' + str(chp.ti.val) + '.')
+        TI.set_attr(P=np.nan)
+        Q1.set_attr(P=chp.Q1.val)
+        self.nw.solve('offdesign', init_path='tmp', design_path='tmp')
+        eq_(round(ti, 1), round(chp.ti.val, 1), 'Value of thermal input must be ' + str(ti) + ', is ' + str(chp.ti.val) + '.')
+        heat1 = chp1_cw.m.val_SI * (chp1_cw.h.val_SI - cw1_chp1.h.val_SI)
+        eq_(round(heat1, 1), round(chp.Q1.val, 1), 'Value of thermal input must be ' + str(heat1) + ', is ' + str(chp.Q1.val) + '.')
+        Q1.set_attr(P=np.nan)
+        Q2.set_attr(P=1.2 * chp.Q2.val)
+        self.nw.solve('offdesign', init_path='tmp', design_path='tmp')
+        # due to characteristic function Q1 is equal to Q2 for this cogeneration unit
+        heat1 = chp1_cw.m.val_SI * (chp1_cw.h.val_SI - cw1_chp1.h.val_SI)
+        eq_(round(heat1, 1), round(chp.Q2.val, 1), 'Value of heat output 2 must be ' + str(heat1) + ', is ' + str(chp.Q2.val) + '.')
+        Q2.set_attr(P=np.nan)
+        Q.set_attr(P=1.5 * chp.Q1.val)
+        self.nw.solve('offdesign', init_path='tmp', design_path='tmp')
+        eq_(round(Q.P.val, 1), round(2 * chp.Q2.val, 1), 'Value of total heat output (' + str(Q.P.val) + ') must be twice as much as value of heat output 2 (' + str(chp.Q2.val) + ').')
+        Q.set_attr(P=np.nan)
+        Qloss.set_attr(P=1e5)
+        self.nw.solve('offdesign', init_path='tmp', design_path='tmp')
+        eq_(round(Qloss.P.val, 1), round(chp.Qloss.val, 1), 'Value of heat loss must be ' + str(Qloss.P.val) + ', is ' + str(chp.Qloss.val) + '.')
+        shutil.rmtree('./tmp', ignore_errors=True)
+
+

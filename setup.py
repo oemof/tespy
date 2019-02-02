c04275cd615191346e1655b2953d962580309769
Francesco Witte <francesco.witte@hs-flensburg.de>
2019-02-02 19:39:23 +0100

    changed version name

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 
 setup(name='TESPy',
-      version='0.1.0 master',
+      version='0.1.0 dev',
       description='Thermal Engineering Systems in Python (TESPy)',
       url='http://github.com/oemof/tespy',
       author='Francesco Witte',

037cc285f98224e96e00e17f285e35c69250744d
Francesco Witte <francesco.witte@hs-flensburg.de>
2019-11-13 07:44:57 +0100

    update init

--- tespy/__init__.py
+++ tespy/__init__.py
@@ -6,6 +6,9 @@ __version__ = '0.2.0 dev'
 
 from tespy.components import characteristics as cmp_char, components as cmp, subsystems as subsys
 from tespy import connections as con
-from tespy.tools import helpers as hlp, logger as logger
+# tespy tools imports
+from tespy.tools import (
+        data_containers, fluid_properties, global_vars, helpers, logger
+        )
 from tespy import networks as nwk
 from tespy import network_reader as nwkr

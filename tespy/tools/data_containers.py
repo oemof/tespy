746d4c9b49a6f92d3a811a7aea43b0c72d4c0253
Francesco Witte <francesco.witte@hs-flensburg.de>
2019-11-21 09:24:52 +0100

    update doc-test and rearrange subclasses to alphabetical order

--- tespy/tools/data_containers.py
+++ tespy/tools/data_containers.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8
 
 """This module contains the data_container class and its children. The
-different ata_containers are used to store component or connection properties.
+different data_containers are used to store component or connection properties.
 
 
 This file is part of project TESPy (github.com/oemof/tespy). It's copyrighted
 by the contributors recorded in the version control history of the file,
-available from its original location tespy/tools/helpers.py
+available from its original location tespy/tools/data_containers.py
 
 SPDX-License-Identifier: MIT
 """
@@ -49,27 +49,31 @@ class data_container:
 
     Example
     -------
-    >>> from tespy.tools import data_containers as dc
-    >>> from tespy.components import components as cmp
-    >>> type(dc.dc_cm(is_set=True))
+    The examples below show the different (sub-)classes of data_containers
+    available.
+
+    >>> from tespy.tools.data_containers import (dc_cc, dc_cm, dc_cp, dc_flu,
+    ... dc_gcp, dc_prop, dc_simple)
+    >>> from tespy.components.piping import pipe
+    >>> type(dc_cm(is_set=True))
     <class 'tespy.tools.data_containers.dc_cm'>
-    >>> type(dc.dc_cc(x=[1, 2, 3, 4], y=[1, 4, 9, 16], is_set=True))
+    >>> type(dc_cc(x=[1, 2, 3, 4], y=[1, 4, 9, 16], is_set=True))
     <class 'tespy.tools.data_containers.dc_cc'>
-    >>> type(dc.dc_cp(val=100, is_set=True, is_var=True, printout=True,
-    ...     max_val=1000, min_val=1))
+    >>> type(dc_cp(val=100, is_set=True, is_var=True, printout=True,
+    ...      max_val=1000, min_val=1))
     <class 'tespy.tools.data_containers.dc_cp'>
-    >>> pipe = cmp.pipe('testpipe', L=100, D=0.5, ks=5e-5)
-    >>> type(dc.dc_gcp(is_set=True, elements=[pipe.L, pipe.D, pipe.ks],
-    ...     method='default'))
-    <class 'tespy.tools.helpdata_containersers.dc_gcp'>
-    >>> type(dc.dc_flu(val={'CO2': 0.1, 'H2O': 0.11, 'N2': 0.75, 'O2': 0.03},
-    ...     val_set={'CO2': False, 'H2O': False, 'N2': False, 'O2': True},
-    ...     balance=False))
+    >>> pipe = pipe('testpipe', L=100, D=0.5, ks=5e-5)
+    >>> type(dc_gcp(is_set=True, elements=[pipe.L, pipe.D, pipe.ks],
+    ...      method='default'))
+    <class 'tespy.tools.data_containers.dc_gcp'>
+    >>> type(dc_flu(val={'CO2': 0.1, 'H2O': 0.11, 'N2': 0.75, 'O2': 0.03},
+    ...      val_set={'CO2': False, 'H2O': False, 'N2': False, 'O2': True},
+    ...      balance=False))
     <class 'tespy.tools.data_containers.dc_flu'>
-    >>> type(dc.dc_prop(val=5, val_SI=500000, val_set=True, unit='bar',
-    ...     unit_set=False, ref=None, ref_set=False))
+    >>> type(dc_prop(val=5, val_SI=500000, val_set=True, unit='bar',
+    ...      unit_set=False, ref=None, ref_set=False))
     <class 'tespy.tools.data_containers.dc_prop'>
-    >>> type(dc.dc_simple(val=5, val_set=False))
+    >>> type(dc_simple(val=5, val_set=False))
     <class 'tespy.tools.data_containers.dc_simple'>
     """
 
@@ -132,153 +136,6 @@ class data_container:
             values.
         """
         return {}
-
-# %%
-
-
-class dc_prop(data_container):
-    r"""
-    Parameters
-    ----------
-    val : float
-        Value in user specified unit (or network unit) if unit is unspecified,
-        default: val=np.nan.
-
-    val0 : float
-        Starting value in user specified unit (or network unit) if unit is
-        unspecified, default: val0=np.nan.
-
-    val_SI : float
-        Value in SI_unit, default: val_SI=0.
-
-    val_set : boolean
-        Has the value for this property been set? default: val_set=False.
-
-    ref : tespy.connections.ref
-        Reference object, default: ref=None.
-
-    ref_set : boolean
-        Has a value for this property been referenced to another connection?
-        default: ref_set=False.
-
-    unit : str
-        Unit for this property, default: ref=None.
-
-    unit : boolean
-        Has the unit for this property been specified manually by the user?
-        default: unit_set=False.
-    """
-    def attr(self):
-        return {'val': np.nan, 'val0': np.nan, 'val_SI': 0, 'val_set': False,
-                'ref': None, 'ref_set': False,
-                'unit': None, 'unit_set': False, 'design': np.nan}
-
-# %%
-
-
-class dc_simple(data_container):
-    r"""
-    Simple data container without data type restrictions to val field.
-
-    Parameters
-    ----------
-    val : no specific datatype
-        Value for the property, no predefined datatype. Unset this property by
-        stating val=np.nan.
-
-    val_set : boolean
-        Has the value for this property been set? default: val_set=False.
-    """
-    def attr(self):
-        return {'val': np.nan, 'val_set': False}
-
-# %%
-
-
-class dc_flu(data_container):
-    r"""
-    Parameters
-    ----------
-
-    val : dict
-        Mass fractions of the fluids in a mixture, default: val={}.
-        Pattern for dictionary: keys are fluid name, values are mass fractions.
-
-    val0 : dict
-        Starting values for mass fractions of the fluids in a mixture,
-        default: val0={}. Pattern for dictionary: keys are fluid name, values
-        are mass fractions.
-
-    val_set : dict
-        Which fluid mass fractions have been set, default val_set={}.
-        Pattern for dictionary: keys are fluid name, values are True or False.
-
-    balance : boolean
-        Should the fluid balance equation be applied for this mixture?
-        default: False.
-    """
-    def attr(self):
-        r"""
-        Return the available attributes for a data_container type object.
-
-        Returns
-        -------
-        out : dict
-            Dictionary of available attributes (dictionary keys) with default
-            values.
-        """
-        return {'val': {}, 'val0': {}, 'val_set': {},
-                'design': collections.OrderedDict(), 'balance': False}
-
-# %%
-
-
-class dc_cp(data_container):
-    r"""
-    Parameters
-    ----------
-
-    val : float
-        Value for this component attribute, default: val=1.
-
-    val_SI : float
-        Value in SI_unit (available for temperatures only, unit transformation
-        according to network's temperature unit), default: val_SI=0.
-
-    is_set : boolean
-        Has the value for this attribute been set?, default: is_set=False.
-
-    is_var : boolean
-        Is this attribute part of the system variables?, default: is_var=False.
-
-    d : float
-        Interval width for numerical calculation of partial derivative towards
-        this attribute, it is part of the system variables, default d=1e-4.
-
-    min_val : float
-        Minimum value for this attribute, used if attribute is part of the
-        system variables, default: min_val=1.1e-4.
-
-    max_val : float
-        Maximum value for this attribute, used if attribute is part of the
-        system variables, default: max_val=1e12.
-
-    printout : boolean
-        Should the value of this attribute be printed in the results overview?
-    """
-    def attr(self):
-        r"""
-        Return the available attributes for a data_container type object.
-
-        Returns
-        -------
-        out : dict
-            Dictionary of available attributes (dictionary keys) with default
-            values.
-        """
-        return {'val': 1, 'val_SI': 0, 'is_set': False, 'printout': True,
-                'd': 1e-4, 'min_val': -1e12, 'max_val': 1e12, 'is_var': False,
-                'val_ref': 1, 'design': np.nan}
 
 # %%
 
@@ -387,6 +244,94 @@ class dc_cm(data_container):
 # %%
 
 
+class dc_cp(data_container):
+    r"""
+    Parameters
+    ----------
+
+    val : float
+        Value for this component attribute, default: val=1.
+
+    val_SI : float
+        Value in SI_unit (available for temperatures only, unit transformation
+        according to network's temperature unit), default: val_SI=0.
+
+    is_set : boolean
+        Has the value for this attribute been set?, default: is_set=False.
+
+    is_var : boolean
+        Is this attribute part of the system variables?, default: is_var=False.
+
+    d : float
+        Interval width for numerical calculation of partial derivative towards
+        this attribute, it is part of the system variables, default d=1e-4.
+
+    min_val : float
+        Minimum value for this attribute, used if attribute is part of the
+        system variables, default: min_val=1.1e-4.
+
+    max_val : float
+        Maximum value for this attribute, used if attribute is part of the
+        system variables, default: max_val=1e12.
+
+    printout : boolean
+        Should the value of this attribute be printed in the results overview?
+    """
+    def attr(self):
+        r"""
+        Return the available attributes for a data_container type object.
+
+        Returns
+        -------
+        out : dict
+            Dictionary of available attributes (dictionary keys) with default
+            values.
+        """
+        return {'val': 1, 'val_SI': 0, 'is_set': False, 'printout': True,
+                'd': 1e-4, 'min_val': -1e12, 'max_val': 1e12, 'is_var': False,
+                'val_ref': 1, 'design': np.nan}
+
+# %%
+
+
+class dc_flu(data_container):
+    r"""
+    Parameters
+    ----------
+
+    val : dict
+        Mass fractions of the fluids in a mixture, default: val={}.
+        Pattern for dictionary: keys are fluid name, values are mass fractions.
+
+    val0 : dict
+        Starting values for mass fractions of the fluids in a mixture,
+        default: val0={}. Pattern for dictionary: keys are fluid name, values
+        are mass fractions.
+
+    val_set : dict
+        Which fluid mass fractions have been set, default val_set={}.
+        Pattern for dictionary: keys are fluid name, values are True or False.
+
+    balance : boolean
+        Should the fluid balance equation be applied for this mixture?
+        default: False.
+    """
+    def attr(self):
+        r"""
+        Return the available attributes for a data_container type object.
+
+        Returns
+        -------
+        out : dict
+            Dictionary of available attributes (dictionary keys) with default
+            values.
+        """
+        return {'val': {}, 'val0': {}, 'val_set': {},
+                'design': collections.OrderedDict(), 'balance': False}
+
+# %%
+
+
 class dc_gcp(data_container):
     r"""
     Parameters
@@ -414,3 +359,66 @@ class dc_gcp(data_container):
             values.
         """
         return {'is_set': False, 'method': 'default', 'elements': []}
+
+# %%
+
+
+class dc_prop(data_container):
+    r"""
+    Parameters
+    ----------
+    val : float
+        Value in user specified unit (or network unit) if unit is unspecified,
+        default: val=np.nan.
+
+    val0 : float
+        Starting value in user specified unit (or network unit) if unit is
+        unspecified, default: val0=np.nan.
+
+    val_SI : float
+        Value in SI_unit, default: val_SI=0.
+
+    val_set : boolean
+        Has the value for this property been set? default: val_set=False.
+
+    ref : tespy.connections.ref
+        Reference object, default: ref=None.
+
+    ref_set : boolean
+        Has a value for this property been referenced to another connection?
+        default: ref_set=False.
+
+    unit : str
+        Unit for this property, default: ref=None.
+
+    unit : boolean
+        Has the unit for this property been specified manually by the user?
+        default: unit_set=False.
+
+    Example
+    -------
+    See :func:`tespy.tools.data_containers.data_container`
+    """
+    def attr(self):
+        return {'val': np.nan, 'val0': np.nan, 'val_SI': 0, 'val_set': False,
+                'ref': None, 'ref_set': False,
+                'unit': None, 'unit_set': False, 'design': np.nan}
+
+# %%
+
+
+class dc_simple(data_container):
+    r"""
+    Simple data container without data type restrictions to val field.
+
+    Parameters
+    ----------
+    val : no specific datatype
+        Value for the property, no predefined datatype. Unset this property by
+        stating val=np.nan.
+
+    val_set : boolean
+        Has the value for this property been set? default: val_set=False.
+    """
+    def attr(self):
+        return {'val': np.nan, 'val_set': False}

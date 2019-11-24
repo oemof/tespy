be7aad891154f81429637af8eb1fa47c885f1529
Francesco Witte <francesco.witte@hs-flensburg.de>
2019-11-24 11:07:23 +0100

    adjust __init__ import order

--- tespy/__init__.py
+++ tespy/__init__.py
@@ -1,17 +1,18 @@
 __version__ = '0.2.0 dev'
 
+# tespy networks imports
+from tespy.networks import networks, network_reader
+
 # tespy components imports
 from tespy.components import (
         basics, combustion, components, heat_exchangers, nodes, piping,
         reactors, subsystems, turbomachinery
         )
 
+# tespy connection imports
 from tespy import connections
 
 # tespy tools imports
 from tespy.tools import (
         data_containers, fluid_properties, global_vars, helpers, logger
         )
-
-from tespy.networks import networks
-from tespy.networks import network_reader

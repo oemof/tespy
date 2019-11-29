a827bc0b33d94dbaf321e1dc991a1c4ee925bda1
Francesco Witte <francesco.witte@hs-flensburg.de>
2019-11-29 11:32:16 +0100

    fix imports for fluid property tests

--- tests/fluid_property_tests.py
+++ tests/fluid_property_tests.py
@@ -2,7 +2,8 @@
 
 from nose.tools import eq_
 
-from tespy import hlp
+from tespy.tools import fluid_properties as fp
+from tespy.tools.global_vars import molar_masses, gas_constants
 from CoolProp.CoolProp import PropsSI as CP
 import numpy as np
 
@@ -11,8 +12,8 @@ class fluid_property_tests:
 
     def setup(self):
         fluids = ['Air', 'N2', 'O2', 'Ar', 'CO2']
-        hlp.memorise.add_fluids(['Air'])
-        hlp.memorise.add_fluids(['N2', 'O2', 'Ar', 'CO2'])
+        fp.memorise.add_fluids(['Air'])
+        fp.memorise.add_fluids(['N2', 'O2', 'Ar', 'CO2'])
 
         mix = {'N2': 0.7556, 'O2': 0.2315, 'Ar': 0.0129}
         pure = {'Air': 1}
@@ -20,20 +21,24 @@ class fluid_property_tests:
         self.flow_pure = [0, 0, 0, pure]
         self.p_range = np.linspace(1e-2, 200, 40) * 1e5
         self.T_range = np.linspace(220, 2220, 40)
-        self.errormsg = 'Relative deviation of fluid mixture to base (CoolProp air) is too high: '
+        self.errormsg = ('Relative deviation of fluid mixture to base '
+                         '(CoolProp air) is too high: ')
 
         for f in fluids:
-            hlp.molar_masses[f] = CP('M', f)
-            hlp.gas_constants[f] = CP('GAS_CONSTANT', f)
+            fp.molar_masses[f] = CP('M', f)
+            fp.gas_constants[f] = CP('GAS_CONSTANT', f)
 
     def test_properties(self):
         """
-        Test fluid properties of dry air fluid mixture calculated by tespy with pseudo pure fluid air from CoolProp.
+        Test gas mixture fluid properties.
+
+        Test the CoolProp pseudo pure fluid dry air properties vs. mixture of
+        air components. Check enthalpy, entropy, specific volume, viscosity.
         """
-        funcs = {'h': hlp.h_mix_pT,
-                 's': hlp.s_mix_pT,
-                 'v': hlp.v_mix_pT,
-                 'visc': hlp.visc_mix_pT}
+        funcs = {'h': fp.h_mix_pT,
+                 's': fp.s_mix_pT,
+                 'v': fp.v_mix_pT,
+                 'visc': fp.visc_mix_pT}
         for name, func in funcs.items():
             # enthalpy and entropy need reference point definition
             if name == 'h' or name == 's':
@@ -51,7 +56,9 @@ class fluid_property_tests:
 
                     # enthalpy and entropy need reference point
                     if name == 'h' or name == 's':
-                        d_rel = abs(((val_mix - mix_ref) - (val_pure - pure_ref)) / (val_pure - pure_ref))
+                        d_rel = abs(((val_mix - mix_ref) -
+                                     (val_pure - pure_ref)) /
+                                    (val_pure - pure_ref))
                     else:
                         d_rel = abs((val_mix - val_pure) / val_pure)
 
@@ -64,8 +71,8 @@ class fluid_property_tests:
 
                     # the deviations might have to be checked
                     if p <= 1e6:
-                        eq_(d_rel < 0.015, True, self.errormsg + 'Value is ' + str(round(d_rel, 4)) +
-                            ' for inputs p=' + str(round(p, 0)) + ', T=' + str(round(T, 0)) + ' for function ' + name + '.')
+                        msg = 'Value is ' +str(round(d_rel, 4)) + ' for inputs p=' + str(round(p, 0)) + ', T=' + str(round(T, 0)) + ' for function ' + name + '.'
+                        eq_(d_rel < 0.015, True, self.errormsg + msg)
                     elif p < 5e6 and T < 500:
                         eq_(d_rel < 0.05, True, self.errormsg + 'Value is ' + str(round(d_rel, 4)) +
                             ' for inputs p=' + str(round(p, 0)) + ', T=' + str(round(T, 0)) + ' for function ' + name + '.')

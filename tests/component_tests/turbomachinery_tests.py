56b5ad34b63d0738f42321ba9ebaf42a69731213
Francesco Witte <francesco.witte@hs-flensburg.de>
2019-11-30 14:18:10 +0100

    udpate pump tests

--- tests/component_tests/turbomachinery_tests.py
+++ tests/component_tests/turbomachinery_tests.py
@@ -5,7 +5,7 @@ from nose.tools import eq_
 from tespy.components.basics import sink, source
 from tespy.components.turbomachinery import (turbomachine, compressor, pump,
                                              turbine)
-from tespy.connections import connection, bus
+from tespy.connections import connection, bus, ref
 from tespy.networks.networks import network
 from tespy.tools.data_containers import dc_cc, dc_cm
 from tespy.tools.fluid_properties import s_mix_ph
@@ -84,7 +84,7 @@ class turbomachinery_tests:
         # going below lowest available speedline, above highest mass flow at
         # that line
         self.c1.set_attr(T=300)
-        self.nw.solve('offdesign', design_path='tmp')
+        self.nw.solve('offdesign', design_path='tmp', init_path='tmp')
         # should be value
         eta_s = eta_s_d * instance.char_map.z2[0, 9]
         msg = ('Value of isentropic efficiency (' + str(instance.eta_s.val) +
@@ -100,16 +100,17 @@ class turbomachinery_tests:
                                            is_set=True, param='m'))
         instance.char_map.is_set = False
         self.nw.solve('offdesign', design_path='tmp')
-        msg = ('Value of isentropic efficiency must be ' + str(eta_s) +
+        msg = ('Value of isentropic efficiency must be ' + str(eta_s_d) +
                 ', is ' + str(instance.eta_s.val) + '.')
-        eq_(round(eta_s, 3), round(instance.eta_s.val, 3), msg)
+        eq_(round(eta_s_d, 3), round(instance.eta_s.val, 3), msg)
 
         # move up in volumetric flow
         self.c1.set_attr(v=1.5)
         self.nw.solve('offdesign', design_path='tmp')
-        eta_s = 0.88
+        eta_s = round(eta_s_d * instance.eta_s_char.func.f_x(
+            self.c1.m.val_SI / self.c1.m.design), 3)
         msg = ('Value of isentropic efficiency must be ' + str(eta_s) +
-                ', is ' + str(instance.eta_s.val) + '.')
+                ', is ' + str(round(instance.eta_s.val, 3)) + '.')
         eq_(eta_s, round(instance.eta_s.val, 3), msg)
 
         # test parameter specification for pr
@@ -117,73 +118,99 @@ class turbomachinery_tests:
         self.c1.set_attr(v=1)
         self.c2.set_attr(p=7.5)
         self.nw.solve('offdesign', design_path='tmp')
-        eta_s = 0.829
+        expr = (self.c2.p.val_SI * self.c1.p.design /
+                (self.c2.p.design * self.c1.p.val_SI))
+        eta_s = round(eta_s_d * instance.eta_s_char.func.f_x(expr), 3)
         msg = ('Value of isentropic efficiency must be ' + str(eta_s) +
-                ', is ' + str(instance.eta_s.val) + '.')
+                ', is ' + str(round(instance.eta_s.val, 3)) + '.')
         eq_(eta_s, round(instance.eta_s.val, 3), msg)
 
         shutil.rmtree('./tmp', ignore_errors=True)
 
-    # def test_pump(self):
-    #     """
-    #     Test component properties of pumps.
-    #     """
-    #     instance = pump('pump')
-    #     c1, c2 = self.setup_network_11(instance)
-    #     fl = {'N2': 0, 'O2': 0, 'Ar': 0, 'INCOMP::DowQ': 1, 'H2O': 0,
-    #           'NH3': 0, 'CO2': 0, 'CH4': 0}
-    #     self.c1.set_attr(fluid=fl, v=1, p=5, T=50)
-    #     self.c2.set_attr(p=7)
-    #     instance.set_attr(eta_s=1)
-    #     self.nw.solve('design')
-    #     # calculate isentropic efficiency the old fashioned way
-    #     eta_s = (instance.h_os('') - self.c1.h.val_SI) / (self.c2.h.val_SI - self.c1.h.val_SI)
-    #     msg = ('Value of isentropic efficiency must be ' + str(eta_s) +
-    #             ', is ' + str(instance.eta_s.val) + '.')
-    #     eq_(eta_s, instance.eta_s.val, msg)
-    #     s1 = round(s_mix_ph(self.c1.to_flow()), 4)
-    #     s2 = round(s_mix_ph(self.c2.to_flow()), 4)
-    #     msg = ('Value of entropy must be identical for inlet (' + str(s1) +
-    #             ') and outlet (' + str(s2) +
-    #             ') at 100 % isentropic efficiency.')
-    #     eq_(s1, s2, msg)
-    #     instance.set_attr(eta_s=0.7)
-    #     self.nw.solve('design')
-    #     self.nw.save('tmp')
-    #     self.c2.set_attr(p=np.nan)
-    #     # flow char (pressure rise vs. volumetric flow)
-    #     x = [0, 0.2, 0.4, 0.6, 0.8, 1, 1.2, 1.4]
-    #     y = np.array([14, 13.5, 12.5, 11, 9, 6.5, 3.5, 0]) * 1e5
-    #     char = dc_cc(x=x, y=y, is_set=True)
-    #     # apply flow char and eta_s char
-    #     instance.set_attr(flow_char=char, eta_s=np.nan,
-    #                       eta_s_char=dc_cc(method='GENERIC', is_set=True))
-    #     self.nw.solve('offdesign', design_path='tmp')
-    #     msg = ('Value of pressure rise must be ' + str(650000) + ', is ' +
-    #             str(self.c2.p.val_SI - self.c1.p.val_SI) + '.')
-    #     eq_(round(self.c2.p.val_SI - self.c1.p.val_SI, 0), 650000, msg)
-    #     self.c1.set_attr(v=0.9)
-    #     self.nw.solve('offdesign', design_path='tmp')
-    #     msg = ('Value of pressure rise must be ' + str(775000.0) + ', is ' +
-    #             str(self.c2.p.val_SI - self.c1.p.val_SI) + '.')
-    #     eq_(self.c2.p.val_SI - self.c1.p.val_SI, 775000.0, msg)
-    #     msg = ('Value of isentropic efficiency must be ' + str(0.694) +
-    #             ', is ' + str(instance.eta_s.val) + '.')
-    #     eq_(0.694, round(instance.eta_s.val, 3), msg)
-    #     instance.eta_s_char.is_set = False
-    #     # test boundaries of characteristic line
-    #     self.c2.set_attr(T=ref(c1, 0, 20))
-    #     self.c1.set_attr(v=-0.1)
-    #     self.nw.solve('design')
-    #     msg = ('Value of power must be ' + str(14e5) + ', is ' +
-    #             str(self.c2.p.val_SI - self.c1.p.val_SI) + '.')
-    #     eq_(self.c2.p.val_SI - self.c1.p.val_SI, 14e5, msg)
-    #     self.c1.set_attr(v=1.5)
-    #     self.nw.solve('design')
-    #     msg = ('Value of power must be ' + str(0) + ', is ' +
-    #             str(self.c2.p.val_SI - self.c1.p.val_SI) + '.')
-    #     eq_(self.c2.p.val_SI - self.c1.p.val_SI, 0, msg)
-    #     shutil.rmtree('./tmp', ignore_errors=True)
+    def test_pump(self):
+        """
+        Test component properties of pumps.
+        """
+        instance = pump('pump')
+        self.setup_network(instance)
+        fl = {'N2': 0, 'O2': 0, 'Ar': 0, 'INCOMP::DowQ': 1, 'H2O': 0,
+              'NH3': 0, 'CO2': 0, 'CH4': 0}
+        self.c1.set_attr(fluid=fl, v=1, p=5, T=50)
+        self.c2.set_attr(p=7)
+        instance.set_attr(eta_s=1)
+        self.nw.solve('design')
+
+        # test calculated value for efficiency
+        eta_s = ((instance.h_os('') - self.c1.h.val_SI) /
+                 (self.c2.h.val_SI - self.c1.h.val_SI))
+        msg = ('Value of isentropic efficiency must be ' + str(eta_s) +
+                ', is ' + str(instance.eta_s.val) + '.')
+        eq_(eta_s, instance.eta_s.val, msg)
+
+        # isentropic efficiency of 1 means inlet and outlet entropy are
+        # identical
+        s1 = round(s_mix_ph(self.c1.to_flow()), 4)
+        s2 = round(s_mix_ph(self.c2.to_flow()), 4)
+        msg = ('Value of entropy must be identical for inlet (' + str(s1) +
+                ') and outlet (' + str(s2) +
+                ') at 100 % isentropic efficiency.')
+        eq_(s1, s2, msg)
+
+        # specify realistic value for efficiency, outlet pressure from flow
+        # char
+        eta_s_d = 0.8
+        instance.set_attr(eta_s=eta_s_d)
+        self.nw.solve('design')
+        self.nw.save('tmp')
+        self.c2.set_attr(p=np.nan)
+
+        # flow char (pressure rise vs. volumetric flow)
+        x = [0, 0.2, 0.4, 0.6, 0.8, 1, 1.2, 1.4]
+        y = np.array([14, 13.5, 12.5, 11, 9, 6.5, 3.5, 0]) * 1e5
+        char = dc_cc(x=x, y=y, is_set=True)
+        # apply flow char and eta_s char
+        instance.set_attr(flow_char=char, eta_s=np.nan,
+                          eta_s_char=dc_cc(method='GENERIC', is_set=True))
+        self.nw.solve('offdesign', design_path='tmp')
+
+        # value for difference pressure
+        dp = 650000.0
+        msg = ('Value of pressure rise must be ' + str(dp) + ', is ' +
+                str(self.c2.p.val_SI - self.c1.p.val_SI) + '.')
+        eq_(round(self.c2.p.val_SI - self.c1.p.val_SI, 0), dp, msg)
+
+        # test ohter volumetric flow on flow char
+        self.c1.set_attr(v=0.9)
+        self.nw.solve('offdesign', design_path='tmp')
+        dp = 775000.0
+        msg = ('Value of pressure rise must be ' + str() + ', is ' +
+                str(self.c2.p.val_SI - self.c1.p.val_SI) + '.')
+        eq_(self.c2.p.val_SI - self.c1.p.val_SI, dp, msg)
+
+        # test value of isentropic efficiency
+        eta_s = round(eta_s_d * instance.eta_s_char.func.f_x(
+            self.c1.v.val_SI / self.c1.v.design), 3)
+        msg = ('Value of isentropic efficiency must be ' + str(eta_s) +
+                ', is ' + str(instance.eta_s.val) + '.')
+        eq_(eta_s, round(instance.eta_s.val, 3), msg)
+        instance.eta_s_char.is_set = False
+
+        # test boundaries of characteristic line:
+        # lower boundary
+        self.c2.set_attr(T=ref(self.c1, 0, 20))
+        self.c1.set_attr(v=-0.1)
+        self.nw.solve('design')
+        msg = ('Value of power must be ' + str(14e5) + ', is ' +
+                str(self.c2.p.val_SI - self.c1.p.val_SI) + '.')
+        eq_(self.c2.p.val_SI - self.c1.p.val_SI, 14e5, msg)
+
+        # upper boundary
+        self.c1.set_attr(v=1.5)
+        self.nw.solve('design')
+        msg = ('Value of power must be ' + str(0) + ', is ' +
+                str(self.c2.p.val_SI - self.c1.p.val_SI) + '.')
+        eq_(self.c2.p.val_SI - self.c1.p.val_SI, 0, msg)
+        shutil.rmtree('./tmp', ignore_errors=True)
 
     # def test_turbine(self):
     #     """

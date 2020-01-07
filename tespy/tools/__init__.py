fc81094e995a659d0a1b296acadc35eac210aad4
Francesco Witte <francesco.witte@hs-flensburg.de>
2020-01-07 11:47:10 +0100

    Add shortcuts to characteristics moduel

--- tespy/tools/__init__.py
+++ tespy/tools/__init__.py
@@ -3,3 +3,10 @@
 from tespy.tools.data_containers import (
     dc_cc, dc_cm, dc_cp, dc_flu, dc_gcp, dc_prop, dc_simple
     )
+
+from tespy.tools.characteristics import(
+    char_line,
+    char_map,
+    load_custom_char,
+    load_default_char
+    )

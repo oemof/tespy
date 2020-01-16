7cc5d5735cb81c96adf48b924e2637bcfc8dc0b0
Francesco Witte <francesco.witte@hs-flensburg.de>
2020-01-16 09:13:12 +0100

    Update version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 
 setup(name='TESPy',
-      version='0.2.0',
+      version='0.2.1 dev',
       description='Thermal Engineering Systems in Python (TESPy)',
       url='http://github.com/oemof/tespy',
       author='Francesco Witte',
--- tespy/__init__.py
+++ tespy/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8
 
-__version__ = '0.2.0 - Clausius\' Circus'
+__version__ = '0.2.1 dev'
 
 # tespy networks imports
 from tespy.networks import (

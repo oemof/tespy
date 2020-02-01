056e17e214db055c9deae71d6b3d1d29519169d9
Francesco Witte <francesco.witte@hs-flensburg.de>
2020-02-01 11:12:43 +0100

    Requirements have version ranges

--- setup.py
+++ setup.py
@@ -20,8 +20,8 @@ setup(name='TESPy',
       data_files=[('tespy/data', ['tespy/data/char_lines.json',
                                   'tespy/data/char_maps.json'])],
       python_requires='>=3',
-      install_requires=['CoolProp >= 6.3.0',
-                        'numpy >= 1.16.2',
-                        'pandas < 1.0.0',
-                        'scipy >= 1.4.1',
-                        'tabulate >= 0.8.6'])
+      install_requires=['CoolProp>=6,<7',
+                        'numpy>=1.13.3,<2',
+                        'pandas>=0.19.2,<1',
+                        'scipy>=0.19.1,<2',
+                        'tabulate>=0.8.2,<0.9'])

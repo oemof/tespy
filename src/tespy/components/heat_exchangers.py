542095f8082d864fdbb568ba08180d8862c0808b
Francesco Witte <francesco.witte@hs-flensburg.de>
2020-11-30 18:59:32 +0100

    Update parameter calculation in postprocessing

--- src/tespy/components/heat_exchangers.py
+++ src/tespy/components/heat_exchangers.py
@@ -640,8 +640,8 @@ class heat_exchanger_simple(component):
         ttd_1 = T_mix_ph(i, T0=self.inl[0].T.val_SI) - self.Tamb.val_SI
         ttd_2 = T_mix_ph(o, T0=self.outl[0].T.val_SI) - self.Tamb.val_SI
 
-        # If one temperature difference is negative: use mean difference to
-        # avoid negative logarithm otherwise use lmtd
+        # For numerical stability: If one temperature difference is negative
+        # use mean difference to avoid negative logarithm otherwise use lmtd.
         if (ttd_1 / ttd_2) < 0:
             td_log = (ttd_2 + ttd_1) / 2
         elif ttd_1 > ttd_2:
@@ -852,14 +852,10 @@ class heat_exchanger_simple(component):
             self.Sirr.val = self.SQ1.val + self.SQ2.val
 
             if not self.kA.is_set:
-                ttd_1 = T_mix_ph(i, T0=self.inl[0].T.val_SI) - \
-                        self.Tamb.val_SI
-                ttd_2 = T_mix_ph(o, T0=self.outl[0].T.val_SI) - \
-                        self.Tamb.val_SI
+                ttd_1 = T_mix_ph(i, T0=self.inl[0].T.val_SI) - self.Tamb.val_SI
+                ttd_2 = T_mix_ph(o, T0=self.outl[0].T.val_SI) - self.Tamb.val_SI
 
-                if (ttd_1 / ttd_2) < 0:
-                    td_log = (ttd_2 + ttd_1) / 2
-                elif ttd_1 > ttd_2:
+                if ttd_1 > ttd_2:
                     td_log = (ttd_1 - ttd_2) / np.log(ttd_1 / ttd_2)
                 elif ttd_1 < ttd_2:
                     td_log = (ttd_2 - ttd_1) / np.log(ttd_2 / ttd_1)
@@ -871,8 +867,8 @@ class heat_exchanger_simple(component):
         if self.kA.is_set:
             # get bound errors for kA characteristic line
             if self.kA_char.param == 'm':
-                self.kA_char.func.get_bound_errors(i[0] / self.inl[0].m.design,
-                                                   self.label)
+                self.kA_char.func.get_bound_errors(
+                    i[0] / self.inl[0].m.design, self.label)
 
         self.check_parameter_bounds()
 

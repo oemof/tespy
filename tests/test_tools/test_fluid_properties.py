abfcb8602d07a2b3275b5f1cc64fc38af7c2124b
Francesco Witte <francesco.witte@hs-flensburg.de>
2021-02-17 09:47:44 +0100

    Find a way to skip test on GH actions

--- tests/test_tools/test_fluid_properties.py
+++ tests/test_tools/test_fluid_properties.py
@@ -11,6 +11,7 @@ SPDX-License-Identifier: MIT
 """
 import os
 import shutil
+import logging
 
 import numpy as np
 import pytest
@@ -311,13 +312,15 @@ class TestFluidPropertyBackEnds:
 
     @pytest.mark.skipif(
         os.environ.get('TRAVIS') == 'true',
-        reason='Travis CI cannot handle the tabular CoolProp back ends, '
+        reason='GitHub actions cannot handle the tabular CoolProp back ends, '
         'skipping this test. The test should run on your local machine.')
     def test_clausius_rankine_tabular(self):
         """Test the Clausius-Rankine cycle with different back ends."""
         fluid = 'water'
+        logging.error(str(os.environ))
         back_ends = ['HEOS', 'BICUBIC', 'TTSE']
         results = {}
+        s
         for back_end in back_ends:
             # delete the fluid from the memorisation class
             if fluid in fp.Memorise.state.keys():

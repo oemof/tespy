f99af108bd2eadd7ee4aaca2381c61c8ac136f9e
Francesco Witte <francesco.witte@hs-flensburg.de>
2021-05-17 16:16:01 +0200

    Fix pandas requirement again

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*names, **kwargs):
 
 setup(
     name='TESPy',
-    version='0.4.3-004',
+    version='0.4.3-005',
     license='MIT',
     description='Thermal Engineering Systems in Python (TESPy)',
     long_description='%s' % (
@@ -67,7 +67,7 @@ setup(
         'CoolProp>=6.4,<7',
         'matplotlib>=3.2.1,<4',
         'numpy>=1.13.3,<2',
-        'pandas>=1.20.0,<2',
+        'pandas>=1.2.0,<2',
         'scipy>=0.19.1,<2',
         'tabulate>=0.8.2,<0.9'
     ],

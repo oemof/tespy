6b1b1bf65212148d7dd85555fa032e4df48661b1
Francesco Witte <github@witte.sh>
2022-03-12 16:17:40 +0100

    More isort

--- src/tespy/tools/helpers.py
+++ src/tespy/tools/helpers.py
@@ -11,13 +11,12 @@ SPDX-License-Identifier: MIT
 import logging
 import os
 
-import CoolProp as CP
-import numpy as np
-
-
 from collections import OrderedDict
 from collections.abc import Mapping
 from copy import deepcopy
+
+import CoolProp as CP
+import numpy as np
 
 from tespy.tools.global_vars import err
 from tespy.tools.global_vars import fluid_property_data

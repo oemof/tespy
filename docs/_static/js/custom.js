2738efe0967466cae92f1e0c8fc5a9631f483596
Francesco Witte <github@witte.sh>
2023-08-18 12:42:45 +0200

    Pin sphinx version and add possiblity to remove banner if empty

--- docs/_static/js/custom.js
+++ docs/_static/js/custom.js
@@ -8,15 +8,28 @@ function includeHTML() {
       xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
         if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          if (this.status == 200) {
+            elmnt.innerHTML = this.responseText;
+            if (elmnt.innerHTML.length === 0) {
+              removeAnnouncementBanner();
+              return;
+            }
+          } else if (this.status == 404) {
+            removeAnnouncementBanner();
+            return;
+          }
           elmnt.removeAttribute("w3-include-html");
           includeHTML();
         }
-      }
+      };
       xhttp.open("GET", file, true);
       xhttp.send();
       return;
     }
   }
+};
+
+function removeAnnouncementBanner() {
+  var banner = document.getElementsByClassName("announcement");
+  banner[0].remove();
 };

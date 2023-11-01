09f487514b13fc48050c4ff93e077b6ef07cdffb
mrk <mrk@ipu.dk>
2023-11-01 15:47:28 +0100

    updating SimpleHeatExchangerDeltaP with new jacobian structure
    (cherry picked from commit cc22938969cb327d8bdcc885346d973cb36fcab1)

--- intermediateFoodTests/newComponentsTests/heatex_alone_deltaP.py
+++ intermediateFoodTests/newComponentsTests/heatex_alone_deltaP.py
@@ -3,7 +3,7 @@
 import logging
 
 
-from tespy.components import HeatExchangerSimple, Source, Sink, Merge, Separator 
+from tespy.components import SimpleHeatExchanger, Source, Sink, Merge, Separator 
 from tespy.tools import ComponentProperties
 from tespy.connections import Connection
 from tespy.networks import Network
@@ -12,22 +12,24 @@ import numpy as np
 from tespy.tools.data_containers import ComponentProperties as dc_cp
 from tespy.tools.data_containers import GroupedComponentProperties as dc_gcp
 
-from tespy.components.newcomponents import HeatExchangerSimpleDeltaP, HeatExchangerSimpleLossFactor,MergeWithPressureLoss,SeparatorWithSpeciesSplits
+from tespy.components.newcomponents import SimpleHeatExchangerDeltaP, SimpleHeatExchangerDeltaPLossFactor,MergeWithPressureLoss,SeparatorWithSpeciesSplits
 
-
+logging.basicConfig(level=logging.DEBUG)
 
 # %%
 
 # caution, must write "Water" (capital W) in INCOMP backend -> CoolProp bug? Intentional?
-fluids = ["INCOMP::FoodWater", "INCOMP::FoodProtein"]
-nw = Network(fluids=fluids, p_unit="bar", T_unit="C")
+#fluids = ["INCOMP::FoodWater", "INCOMP::FoodProtein"]
+#nw = Network(p_unit="bar", T_unit="C", iterinfo=True)
+nw = Network(m_unit='kg / s', p_unit='bar', T_unit='C',h_unit='kJ / kg', h_range=[-1e2,4e2], iterinfo=True)
+
 
 so = Source("Source")
 #  Variant 2: Q is m (h_2 - h_1), Q_total is taking efficiency into account and represents the heat transfer over system
 # boundary. For heat transfer into the system: Q = Q_total * eta, for heat transfer from the system: Q_total = Q * eta
 
-#he = HeatExchangerSimpleLossFactor("Heater")
-he = HeatExchangerSimpleDeltaP("Heater")
+#he = SimpleHeatExchangerLossFactor("Heater")
+he = SimpleHeatExchangerDeltaP("Heater")
 
 
 si = Sink("Sink")
@@ -38,11 +40,14 @@ c2 = Connection(he, "out1", si, "in1", label="4")
 nw.add_conns(c1, c2)
 
 # set some generic data for starting values
-c1.set_attr(m=1, p=2.2, T=30, fluid={"FoodWater": 0.9, "FoodProtein": 0.1})
-c2.set_attr(T=50)
+c1.set_attr(fluid={'INCOMP::Water': 0.80,'INCOMP::PHE': 0.15,'INCOMP::S800': 0.05}, m=1, T=30, p=1, mixing_rule="incompressible")
+#c1.set_attr(m=1, p=2.2, T=30, fluid={"INCOMP::FoodWater": 0.9, "INCOMP::FoodProtein": 0.1}, mixing_rule="incompressible")
+c2.set_attr(T=95) #,p=1)
 
 # set pressure ratios of heater and merge
-he.set_attr(deltaP=1)
+he.set_attr(deltaP=0.1)
+#he.set_attr(pr=1.1)
+
 
 #he.set_attr(LF=0.1) # MRK so eta is (1-hlf) heat loss factor
 #he.set_attr(Q_total=86371.13607253956) # MRK so eta is (1-hlf) heat loss factor

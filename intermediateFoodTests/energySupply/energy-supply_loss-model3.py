230c7a7ae9a02937f2276d3a25580296c1a62fa2
mrk <mrk@ipu.dk>
2023-11-02 21:25:14 +0100

    working energy models and examples
    (cherry picked from commit f76274647c97e44b50b0c3ce59567bf105de2289)

--- intermediateFoodTests/energySupply/energy-supply_loss-model3.py
+++ intermediateFoodTests/energySupply/energy-supply_loss-model3.py
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from tespy.components import Separator,Merge,CycleCloser,Valve,Splitter
-from tespy.components.newcomponents import *
+from tespy.components.energySupplyComponents import *
 
 import logging
 logging.basicConfig(level=logging.DEBUG)
@@ -16,7 +16,7 @@ from CoolProp.CoolProp import PropsSI
 
 
 
-fluid_list = ['INCOMP::FoodWater']
+fluid_list = ['INCOMP::Water']
 network = Network(fluids=fluid_list, m_unit='kg / s', p_unit='bar', T_unit='C',h_unit='kJ / kg', h_range=[-1e2,4e3], iterinfo=True)
 
 # Objects
@@ -37,14 +37,12 @@ c1.set_attr(m=m0)
 #c2.set_attr(m=m0*0.9)
 LossModel.set_attr(Loss=0.1)
 
-
-
 # guess
 for c in network.conns['object']:
-    c.set_attr(m0=m0,h0=0,p0=1,fluid0={'FoodWater': 1})
+    c.set_attr(m0=m0,h0=0,p0=1,fluid0={'INCOMP::Water': 1})
 
 # arbitray values
-c1.set_attr(h=0,p=1,fluid={'FoodWater': 1})
+c1.set_attr(h=0,p=1,fluid={'INCOMP::Water': 1}, mixing_rule="incompressible")
 c2.set_attr(h=0,p=1)
 c3.set_attr(h=0,p=1)
 

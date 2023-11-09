804421ccb51cc986713c4a2da73c2e747048eb44
mrk <mrk@ipu.dk>
2023-11-09 12:06:44 +0100

    setting lower bounds, and typo fix

--- incompressiblesTests/newComponentsTests/2streamHX.py
+++ incompressiblesTests/newComponentsTests/2streamHX.py
@@ -29,10 +29,17 @@ cold_in_he = Connection(cold_i, 'out1', he, 'in2')
 cold_he_out = Connection(he, 'out2', cold_o, 'in1')
 nw.add_conns(hot_in_he, hot_he_out, cold_in_he, cold_he_out)
 
+for c in nw.conns['object']:
+    n_fl=3
+    c.set_attr(m0=0.5,h0=1e2,p0=3, mixing_rule="incompressible")
+    c.set_attr(fluid0={"HEOS::Water": 1/n_fl, 'INCOMP::PHE': 1/n_fl, 'INCOMP::S800': 1/n_fl})
+    #c.set_attr(fluid0={'INCOMP::Water': 0.80,'INCOMP::PHE': 0.15,'INCOMP::S800': 0.05})
+    c.set_attr(force_state='l')
+    c.set_attr(good_starting_values=True)
 
 he.set_attr(pr1=1, pr2=1)
-cold_in_he.set_attr(fluid={'water': 1}, T=10, m=1, p=3)
-hot_in_he.set_attr(fluid={'water': 1}, T=35, m=0.5, p=3)
+cold_in_he.set_attr(fluid={'HEOS::Water': 1,'INCOMP::PHE': 0,'INCOMP::S800': 0}, T=1, m=1, p=3,mixing_rule="incompressible")
+hot_in_he.set_attr(fluid={'HEOS::Water': 0.8,'INCOMP::PHE': 0.15,'INCOMP::S800': 0.05}, T=35, m=0.5, p=3)
 hot_he_out.set_attr(T=30)
 
 nw.solve("design",print_results=True)
@@ -60,9 +67,36 @@ if not nw.converged:
 nw.print_results(colored=True, print_results=False) 
 print(nw.results['Connection'][[c for c in nw.results['Connection'].columns if not (c[-4:]=="unit" or c in ["v","Td_bp","s"])]])
 
+he.set_attr(ttd_l = None,ttd_min = None,kA=10e3)    # using new model with pinch
+nw.solve("design",print_results=True)
+if not nw.converged:
+    raise Exception("not converged")
+nw.print_results(colored=True, print_results=False) 
+print(nw.results['Connection'][[c for c in nw.results['Connection'].columns if not (c[-4:]=="unit" or c in ["v","Td_bp","s"])]])
 
 
+# switch hot and cold 
+cold_in_he.set_attr(T=50, m=1, p=3,mixing_rule="incompressible")
+hot_in_he.set_attr(T=20, m=0.5, p=3)
+hot_he_out.set_attr(T=25)
+he.set_attr(ttd_l = None,ttd_min = None,kA=None)    # using new model with pinch
+nw.solve("design",print_results=True)
+if not nw.converged:
+    raise Exception("not converged")
+nw.print_results(colored=True, print_results=False) 
+print(nw.results['Connection'][[c for c in nw.results['Connection'].columns if not (c[-4:]=="unit" or c in ["v","Td_bp","s"])]])
 
+# why negative Q and ttd_u and ttd_l
+
+cold_in_he.set_attr(T=50, m=1, p=3,mixing_rule="incompressible")
+hot_in_he.set_attr(T=20, m=0.5, p=3)
+hot_he_out.set_attr(T=None)
+he.set_attr(ttd_l = None,ttd_min = 5)    # using new model with pinch
+nw.solve("design",print_results=True)
+if not nw.converged:
+    raise Exception("not converged")
+nw.print_results(colored=True, print_results=False) 
+print(nw.results['Connection'][[c for c in nw.results['Connection'].columns if not (c[-4:]=="unit" or c in ["v","Td_bp","s"])]])
 
 
 

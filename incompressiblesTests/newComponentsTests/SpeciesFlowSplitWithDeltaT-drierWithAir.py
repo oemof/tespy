ef5b5ec64fae4956aca904e435b587d5fcc96599
mrk <mrk@ipu.dk>
2023-11-10 13:37:22 +0100

    a drier model

---
+++ incompressiblesTests/newComponentsTests/SpeciesFlowSplitWithDeltaT-drierWithAir.py
@@ -0,0 +1,62 @@
+import logging
+
+from tespy.components import HeatExchangerSimple, Source, Sink, Merge, Separator 
+from tespy.tools import ComponentProperties
+from tespy.connections import Connection
+from tespy.networks import Network
+import numpy as np
+
+from tespy.tools.data_containers import ComponentProperties as dc_cp
+from tespy.tools.data_containers import GroupedComponentProperties as dc_gcp
+
+from tespy.components.newcomponents import \
+    DiabaticSimpleHeatExchanger,MergeDeltaP,SeparatorWithSpeciesSplits, \
+        SeparatorWithSpeciesSplitsDeltaT,DrierWithAir
+
+logging.basicConfig(level=logging.DEBUG)
+
+# %%
+
+# caution, must write "Water" (capital W) in INCOMP backend -> CoolProp bug? Intentional?
+#fluids = ["INCOMP::Water", "INCOMP::T66"]
+nw = Network(m_unit='kg / s', p_unit='bar', T_unit='C',h_unit='kJ / kg', h_range=[-1e2,4e3], iterinfo=True)
+
+so = Source("Source")
+soAir = Source("SourceAir")
+se = DrierWithAir("Separator",num_out=2,num_in=2)
+#se = SeparatorWithSpeciesSplits("Separator") #,num_out=2)
+si1 = Sink("Sink 1")
+si2 = Sink("Sink 2")
+
+c1 = Connection(so, "out1", se, "in1", label="1")
+c2 = Connection(se, "out1", si1, "in1", label="2")
+c3 = Connection(se, "out2", si2, "in1", label="3")
+c4 = Connection(soAir, "out1", se, "in2", label="4")
+
+nw.add_conns(c1, c2, c3, c4)
+
+
+# set some generic data for starting values
+c1.set_attr(m=1, p=1.2, T=50, fluid={"INCOMP::Water": 0.9, "INCOMP::T66": 0.1, "HEOS::Air": 0}, mixing_rule="incompressible")
+c4.set_attr(m=10, p=1.2, T=80, fluid={"INCOMP::Water": 0, "INCOMP::T66": 0, "HEOS::Air": 1}, mixing_rule="incompressible")
+
+c2.set_attr(fluid={"INCOMP::T66": 0})
+c3.set_attr(fluid={"INCOMP::Water": 0.08, "HEOS::Air": 0})
+
+c2.set_attr(p=1.2,T=60)
+c3.set_attr(p=1.2,T=60)
+
+
+nw.solve("design")
+if not nw.converged:
+    raise Exception("not converged")
+nw.print_results()
+print(nw.results['Connection'])
+m_T66_c1 = c1.m.val * c1.fluid.val['T66']
+m_T66_c2 = c2.m.val * c2.fluid.val['T66']
+print(f"\n Species flow split is {m_T66_c2/m_T66_c1}")
+print(f"\n")
+
+
+
+

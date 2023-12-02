652c1290f8d80cd4ed811e23b0cab841a2837c23
Francesco Witte <github@witte.sh>
2023-12-02 11:15:54 +0100

    Ignore link check

--- docs/conf.py
+++ docs/conf.py
@@ -148,4 +148,5 @@ linkcheck_ignore = [
   r'https://doi.org/20.500.12738/6561',  # 08.01.2023: does not resolve anymore
   r'https://doi.org/10.1021/ie4033999',  # 28.02.2023: does not resolve anymore for some reason
   r'https://doi.org/10.1002/bbpc.19900940121',  # 13.03.2023 ...?
+  r' http://www.coolprop.org/fluid_properties/Incompressibles.html',  # 02.12.2023 ...?
 ]

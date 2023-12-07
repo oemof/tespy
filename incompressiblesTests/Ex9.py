23e56e7c62ff11310682be09a579be4efee061c9
mrk <mrk@ipu.dk>
2023-12-07 20:07:08 +0100

    sankey diagram sk

--- incompressiblesTests/Ex9.py
+++ incompressiblesTests/Ex9.py
@@ -140,3 +140,232 @@ print(f"\n")
 print(f"Total heat for boiler is {boiler.Q.val/(3.6*1e6):.1f}")
 print(f"Total heat for presswater heater is {presswaterheater.Q.val/(3.6*1e6):.1f}")
 
+
+
+def get_paths(self):
+    def generate_paths(start, current_comp_path, current_conn_path):
+        # If the component is not in the connections, append the current path, we are finished 
+        if start not in comp_connections:
+            comps_paths.append(current_comp_path)
+            conns_paths.append(current_conn_path)
+            return
+        # Iterate through the connected lists
+        for comp_list,conn_list in zip(comp_connections[start],conn_connections[start]):
+            # Recursively generate paths
+            generate_paths(comp_list[-1], current_comp_path + comp_list[1:], current_conn_path + conn_list)        
+
+    # make lists of component and connection branches
+    comp_branches = [b['components'] for b in self.nw.massflow_branches]
+    conn_branches = [b['connections'] for b in self.nw.massflow_branches]
+    
+    # make connections dict to iterate over 
+    comp_connections = {}
+    conn_connections = {}
+    for comps,conns in zip(comp_branches,conn_branches):
+        if comps[0] not in comp_connections:
+            comp_connections[comps[0]] = []
+            conn_connections[comps[0]] = []
+        comp_connections[comps[0]].append(comps)
+        conn_connections[comps[0]].append(conns)
+
+    # get sources to start from and iterate for each uniquie path
+    sources = [k for k in comp_connections.keys() if type(k).__name__ == "Source"]
+    comps_paths = []
+    conns_paths = []
+    for start in sources:
+        generate_paths(start, [start], [])
+
+    paths = {}
+    paths['connections'] = sorted(conns_paths, key=len, reverse=True)
+    paths['components'] = sorted(comps_paths, key=len, reverse=True)
+    
+    return paths
+
+class SELF():
+    pass
+    
+self = SELF()
+self.nw = network
+
+paths = get_paths(self)
+
+print(paths['components'])
+
+longest_path_components = max(paths['components'], key=len)
+longest_path_components_label = [l.label for l in longest_path_components]
+
+path = {}
+path["T"] = []
+path["source"] = []
+path["source_id"] = []
+path["target"] = []
+for conns in paths['connections']:
+    T = []
+    source  = []
+    source_id = []
+    target  = []    
+    for c in conns:
+        T += [c.T.val]
+        source += [c.source.label] 
+        source_id += [longest_path_components_label.index(c.source.label)]
+        target += [c.target.label] 
+    path["T"].append(T)
+    path["source"].append(source)
+    path["source_id"].append(source_id)
+    
+    path["target"].append(target)
+
+longest_path_components_label
+
+
+import matplotlib.pyplot as plt
+
+plt.figure()
+for s,T in zip(path["source_id"],path["T"]):
+    #x = np.array([i for i,_ in enumerate(T)])
+    #y = np.array(T)
+    x = [longest_path_components_label[i] for i in s]
+    plt.plot(x, T)
+    
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.legend()
+plt.show()
+
+
+print(x)
+
+print(x)
+print(x)
+print(x)
+
+import sys
+sys.exit()
+
+
+
+
+sankey_nodes = []
+for name,comp in self.nw.comps.iterrows():
+    sankey_nodes += [name]
+
+#missing = [value for value in sankey_nodes if not value in longest_path_components_label]
+#sankey_nodes = longest_path_components_label + missing
+
+
+links = {
+    'source': [],
+    'target': [],
+    'value': [],
+    #'color': []
+}
+
+
+for i,c in self.nw.conns.iterrows():
+    links["source"] += [sankey_nodes.index(c.source.label)]
+    links["target"] += [sankey_nodes.index(c.target.label)]
+    links["value"]  += [c['object'].m.val_SI]
+    #links["color"]  
+    #print(c)
+
+from plotly.offline import plot
+import plotly.graph_objects as go
+import plotly as px
+
+
+
+fig = go.Figure(go.Sankey(
+    #arrangement="snap",
+    node = dict(
+      pad = 100,
+      thickness = 20,
+      line = dict(color = "grey", width = 0.5),
+      label = sankey_nodes,
+      color = "blue"
+    ),
+    # node={
+    #     "label": sankey_nodes,
+    #     'pad': 10,
+    #     'thickness': 10,
+    #     'color': 'orange'},
+    link=links))
+plot(fig, filename='R410A_sankey.html')
+
+
+print("hey")
+
+
+# "----------------------------------------------------------------------------------"
+# "----------------------------------------------------------------------------------"
+# "----------------------------------------------------------------------------------"
+# "----------------------------------------------------------------------------------"
+
+
+
+
+sankey_nodes = []
+for name,comp in self.nw.comps.iterrows():
+    sankey_nodes += [name]
+
+#missing = [value for value in sankey_nodes if not value in longest_path_components_label]
+#sankey_nodes = longest_path_components_label + missing
+
+links = {
+    'source': [],
+    'target': [],
+    'value': [],
+    'color': []
+}
+
+#colors = px.colors.DEFAULT_PLOTLY_COLORS
+colors = ["rgba(242, 142, 43, 0.75)",
+          "rgba(118, 183, 178, 0.75)",
+          "rgba(176, 122, 161, 0.75)",
+          "rgba(156, 117, 95, 0.75)",
+          "rgba(237, 201, 72, 0.75)",
+          "rgba(186, 176, 172, 0.75)",
+          "rgba(89, 161, 79, 0.75)",
+          "rgba(255, 157, 167, 0.75)",
+          "rgba(78, 121, 167, 0.75)",
+          "rgba(225, 87, 89, 0.75)",
+          "rgba(100, 100, 100, 1.00)"]
+
+for fluid in self.nw.all_fluids:
+    
+    for i,c in self.nw.conns.iterrows():
+        links["source"] += [sankey_nodes.index(c.source.label)]
+        links["target"] += [sankey_nodes.index(c.target.label)]
+        links["value"]  += [c['object'].m.val_SI * c['object'].fluid.val[fluid]]
+        links["color"]  += [colors[list(self.nw.all_fluids).index(fluid)]]
+        #links["color"]  
+        #print(c)
+
+from plotly.offline import plot
+import plotly.graph_objects as go
+
+fig = go.Figure(go.Sankey(
+    #arrangement="snap",
+    node = dict(
+      pad = 200,
+      thickness = 10,
+      #line = dict(color = "grey", width = 0.5),
+      label = sankey_nodes,
+      color = "grey"
+    ),
+    # node={
+    #     "label": sankey_nodes,
+    #     'pad': 10,
+    #     'thickness': 10,
+    #     'color': 'orange'},
+    link=links))
+plot(fig, filename='R410A_sankey.html')
+
+
+print("hey")
+
+
+
+
+
+
+

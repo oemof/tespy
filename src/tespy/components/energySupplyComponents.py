57b96038e627c32cfc2a52238e612e067d89fbe6
mrk <mrk@ipu.dk>
2024-02-06 13:19:20 +0100

    boiler, compressor, ref unit and heat pump with usefull ratios

--- src/tespy/components/energySupplyComponents.py
+++ src/tespy/components/energySupplyComponents.py
@@ -12,7 +12,8 @@ from tespy.tools.data_containers import SimpleDataContainer as dc_simple
 # Fictious Energy Supply models (energy flows modelled as mass flows)
 # No real use for tespy I guess
 
-class MassFactorEnergySupply(Splitter):
+
+class VaporCompressionSystemEnergySupply(Splitter):
 
     @staticmethod
     def component():
@@ -79,6 +80,150 @@ class MassFactorEnergySupply(Splitter):
         self.Heating.val = self.outl[0].m.val_SI
         self.Cooling.val = self.outl[1].m.val_SI
 
+
+
+class HeatPumpEnergySupply(Splitter):
+
+    """
+    COP sets self.outl[0].m.val_SI
+    UsefullCoolingRatio sets self.outl[1].m.val_SI
+    """
+
+    @staticmethod
+    def component():
+        return 'mass factor vapor compression cycle using COP for converting electricity to heat and cooling (energy flows modelled using tespy mass balances)'
+
+    def get_parameters(self):
+        variables = super().get_parameters()
+        variables["COP"] = dc_cp(
+            min_val=0,
+            deriv=self.COP_deriv,
+            func=self.COP_func,
+            latex=self.mass_flow_func_doc,
+            num_eq=1
+        )
+        variables["UsefullCoolingRatio"] = dc_cp(
+            min_val=0,
+            deriv=self.usefull_deriv,
+            func=self.usefull_func,
+            latex=self.mass_flow_func_doc,
+            num_eq=1
+        )
+        variables["NonUseFullCooling"] = dc_cp(min_val=0, is_result=True)
+        variables['Heating'] = dc_cp(min_val=0, is_result=True)
+        variables['Cooling'] = dc_cp(min_val=0, is_result=True)
+        return variables
+
+    def get_mandatory_constraints(self):
+        constraints = super().get_mandatory_constraints()
+        del constraints['pressure_constraints']
+        del constraints['energy_balance_constraints']
+        del constraints['mass_flow_constraints']
+        return constraints   
+
+    def COP_func(self):
+        return self.inl[0].m.val_SI * self.COP.val - self.outl[0].m.val_SI
+
+    def COP_deriv(self, increment_filter, k):
+        inl = self.inl[0]
+        outl = self.outl[0]
+        if inl.m.is_var:
+            self.jacobian[k, inl.m.J_col] = self.COP.val
+        if outl.m.is_var:
+            self.jacobian[k, outl.m.J_col] = -1
+
+    def usefull_func(self):
+        return self.inl[0].m.val_SI * (self.COP.val-1) * self.UsefullCoolingRatio.val + self.outl[1].m.val_SI # cooling negative
+
+    def usefull_deriv(self, increment_filter, k):
+        inl = self.inl[0]
+        outl = self.outl[1]
+        if inl.m.is_var:
+            self.jacobian[k, inl.m.J_col] = (self.COP.val-1) * self.UsefullCoolingRatio.val 
+        if outl.m.is_var:
+            self.jacobian[k, outl.m.J_col] = 1       
+
+    def calc_parameters(self):
+        super().calc_parameters()
+        # if not self.COP.is_set:
+        #     self.COP.val = self.outl[0].m.val_SI / (self.outl[0].m.val_SI - (-self.outl[1].m.val_SI))
+        self.Heating.val = self.outl[0].m.val_SI
+        self.Cooling.val = self.outl[1].m.val_SI
+        self.NonUseFullCooling.val = self.inl[0].m.val_SI * (self.COP.val - 1) * (1 - self.UsefullCoolingRatio.val)
+
+
+class RefUnitEnergySupply(Splitter):
+
+    """
+    COP sets self.outl[1].m.val_SI
+    UsefullHeatingRatio sets self.outl[0].m.val_SI
+    """
+
+
+    @staticmethod
+    def component():
+        return 'mass factor vapor compression cycle using COP for converting electricity to heat and cooling (energy flows modelled using tespy mass balances)'
+
+    def get_parameters(self):
+        variables = super().get_parameters()
+        variables["COP"] = dc_cp(
+            min_val=0,
+            deriv=self.COP_deriv,
+            func=self.COP_func,
+            latex=self.mass_flow_func_doc,
+            num_eq=1
+        )
+        variables["UsefullHeatingRatio"] = dc_cp(
+            min_val=0,
+            deriv=self.usefull_deriv,
+            func=self.usefull_func,
+            latex=self.mass_flow_func_doc,
+            num_eq=1
+        )
+        variables["NonUseFullHeating"] = dc_cp(min_val=0, is_result=True)        
+        variables['Heating'] = dc_cp(min_val=0, is_result=True)
+        variables['Cooling'] = dc_cp(min_val=0, is_result=True)
+        return variables
+
+    def get_mandatory_constraints(self):
+        constraints = super().get_mandatory_constraints()
+        del constraints['pressure_constraints']
+        del constraints['energy_balance_constraints']
+        del constraints['mass_flow_constraints']
+        return constraints   
+
+    def COP_func(self):
+        return self.inl[0].m.val_SI * self.COP.val + self.outl[1].m.val_SI # cooling negative
+
+    def COP_deriv(self, increment_filter, k):
+        inl = self.inl[0]
+        outl = self.outl[1]
+        if inl.m.is_var:
+            self.jacobian[k, inl.m.J_col] = self.COP.val
+        if outl.m.is_var:
+            self.jacobian[k, outl.m.J_col] = 1
+
+    def usefull_func(self):
+        return self.inl[0].m.val_SI * (self.COP.val + 1) * self.UsefullHeatingRatio.val - self.outl[0].m.val_SI
+
+    def usefull_deriv(self, increment_filter, k):
+        inl = self.inl[0]
+        outl = self.outl[0]
+        if inl.m.is_var:
+            self.jacobian[k, inl.m.J_col] =  (self.COP.val + 1) * self.UsefullHeatingRatio.val
+        if outl.m.is_var:
+            self.jacobian[k, outl.m.J_col] = -1       
+
+    def calc_parameters(self):
+        super().calc_parameters()
+        # if not self.COP.is_set:
+        #     self.COP.val = self.outl[0].m.val_SI / (self.outl[0].m.val_SI - (-self.outl[1].m.val_SI))
+        self.Heating.val = self.outl[0].m.val_SI
+        self.Cooling.val = self.outl[1].m.val_SI
+        self.NonUseFullHeating.val = self.inl[0].m.val_SI * (self.COP.val + 1) * (1 - self.UsefullHeatingRatio.val)
+
+
+
 class MassLossEnergySupply(Splitter):
 
     @staticmethod
@@ -121,7 +266,7 @@ class MassLossEnergySupply(Splitter):
             self.jacobian[k, inl.m.J_col] = (1-self.LossRatio.val)
         for o in self.outl:
             if o.m.is_var:
-                self.jacobian[k, o.m.J_col] = -1
+                self.jacobian[k, o.m.J_col] = -1      
 
     def calc_parameters(self):
         super().calc_parameters()
@@ -182,6 +327,86 @@ class BoilerEffEnergySupply(Splitter):
             self.BoilerEff.val = mout/self.inl[0].m.val_SI
         self.EnergyLoss.val = self.inl[0].m.val_SI - mout
         self.Energy.val = mout
+
+
+
+class BoilerEffUsefullLossEnergySupply(Splitter):
+
+    """
+    BoilerEff defines self.outl[0].m.val_SI
+    UsefullLossRatio defines self.outl[1].m.val_SI
+    """
+
+    @staticmethod
+    def component():
+        return 'mass efficiency model for splitting energy flows (modelled using tespy mass balances)'
+
+    def get_parameters(self):
+        variables = super().get_parameters()
+        variables["BoilerEff"] = dc_cp(
+            min_val=0,
+            deriv=self.Eff_deriv,
+            func=self.Eff_func,
+            latex=self.mass_flow_func_doc,
+            num_eq=1
+        )
+        variables["UsefullLossRatio"] = dc_cp(
+            min_val=0,
+            deriv=self.usefull_deriv,
+            func=self.usefull_func,
+            latex=self.mass_flow_func_doc,
+            num_eq=1
+        )        
+        variables['Energy'] = dc_cp(min_val=0, is_result=True)           
+        variables['EnergyLoss'] = dc_cp(min_val=0, is_result=True)           
+        variables['EnergyLossUsefull'] = dc_cp(min_val=0, is_result=True)           
+        return variables
+    
+    def outlets(self):
+        if self.num_out.is_set:
+            return ['out' + str(i + 1) for i in range(self.num_out.val)]
+        else:
+            self.set_attr(num_out=2)
+            return self.outlets() 
+
+    def get_mandatory_constraints(self):
+        constraints = super().get_mandatory_constraints()
+        del constraints['mass_flow_constraints']
+        del constraints['pressure_constraints']
+        del constraints['energy_balance_constraints']
+        return constraints   
+
+    def Eff_func(self):
+        return self.inl[0].m.val_SI * self.BoilerEff.val - self.outl[0].m.val_SI
+
+    def Eff_deriv(self, increment_filter, k):
+        inl = self.inl[0]
+        outl = self.outl[0]
+        if inl.m.is_var:
+            self.jacobian[k, inl.m.J_col] = self.BoilerEff.val
+        if outl.m.is_var:
+            self.jacobian[k, outl.m.J_col] = -1
+
+    def usefull_func(self):
+        return self.inl[0].m.val_SI * (1-self.BoilerEff.val) * self.UsefullLossRatio.val - self.outl[1].m.val_SI
+
+    def usefull_deriv(self, increment_filter, k):
+        inl = self.inl[0]
+        outl = self.outl[1]
+        if inl.m.is_var:
+            self.jacobian[k, inl.m.J_col] = (1-self.BoilerEff.val) * self.UsefullLossRatio.val
+        if outl.m.is_var:
+            self.jacobian[k, outl.m.J_col] = -1
+
+    def calc_parameters(self):
+        super().calc_parameters()
+        # mout = sum([o.m.val_SI for o in self.outl])
+        # if not self.BoilerEff.is_set:
+        #     self.BoilerEff.val = mout/self.inl[0].m.val_SI
+        self.EnergyLossUsefull.val = self.outl[1].m.val_SI
+        self.EnergyLoss.val = self.inl[0].m.val_SI - sum([o.m.val_SI for o in self.outl])
+        self.Energy.val = self.outl[0].m.val_SI
+
 
 
 class MergeEnergySupply(Merge):

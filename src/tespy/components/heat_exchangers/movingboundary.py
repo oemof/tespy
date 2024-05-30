46db812361587d404298b145a24a8ef99a649bec
Francesco Witte <github@witte.sh>
2024-05-30 05:30:48 +0200

    Add a second draft component
    The `MovingBoundaryHeatExchanger` is the more generic version of
    the first implementation. It allows for any number of sections and
    eventually will also be implemented for evaporation.
    
    The main difference to the first draft of the
    `MovingBoundaryCondenser` is, that it is nondimensional to U or A.
    The sum of all UA values in the sections has to be equal to the UA
    value originally calculated in the design phase. An adoption can
    be made to this later, modifying the total UA value by lookup
    tables similar to the ´kA_char´ approaches in the already existing
    heat exchanger components.

--- src/tespy/components/heat_exchangers/movingboundary.py
+++ src/tespy/components/heat_exchangers/movingboundary.py
@@ -19,6 +19,172 @@ from tespy.tools.fluid_properties import T_mix_ph
 from tespy.tools.fluid_properties import h_mix_pQ
 
 
+class MovingBoundaryHeatExchanger(HeatExchanger):
+
+    @staticmethod
+    def component():
+        return 'moving boundary heat exchanger'
+
+    def get_parameters(self):
+        params = super().get_parameters()
+        params.update({
+            'UA': dc_cp(
+                min_val=0, num_eq=1, func=self.UA_func, deriv=self.UA_deriv
+            ),
+            'td_pinch': dc_cp(
+                min_val=0, num_eq=1, func=self.td_pinch_func,
+                deriv=self.td_pinch_deriv, latex=None
+            )
+        })
+        return params
+
+    def calc_UA_in_sections(self):
+        i1, i2 = self.inl
+        o1, o2 = self.outl
+        h_sat_gas = h_mix_pQ(o1.p.val_SI, 1, o1.fluid_data)
+        h_sat_liquid = h_mix_pQ(o1.p.val_SI, 0, o1.fluid_data)
+
+        if i1.h.val_SI > h_sat_gas and o1.h.val_SI < h_sat_liquid:
+            h_at_steps_1 = [i1.h.val_SI, h_sat_gas, h_sat_liquid, o1.h.val_SI]
+            sections = 3
+
+        elif ((i1.h.val_SI > h_sat_gas) ^ (o1.h.val_SI < h_sat_liquid)):
+            sections = 2
+            if i1.h.val_SI > h_sat_gas:
+                h_at_steps_1 = [i1.h.val_SI, h_sat_gas, o1.h.val_SI]
+            else:
+                h_at_steps_1 = [i1.h.val_SI, h_sat_liquid, o1.h.val_SI]
+
+        else:
+            sections = 1
+            h_at_steps_1 = [i1.h.val_SI, o1.h.val_SI]
+
+        p_at_steps_1 = [i1.p.val_SI for _ in range(sections + 1)]
+        T_at_steps_1 = [T_mix_ph(p, h, i1.fluid_data) for p, h in zip(p_at_steps_1, h_at_steps_1)]
+
+        Q_in_sections = [i1.m.val_SI * (h_at_steps_1[i + 1] - h_at_steps_1[i]) for i in range(sections)]
+
+        h_at_steps_2 = [i2.h.val_SI]
+        for Q in Q_in_sections:
+            h_at_steps_2.append(h_at_steps_2[-1] + abs(Q) / i2.m.val_SI)
+
+        p_at_steps_2 = [i2.p.val_SI for _ in range(sections + 1)]
+        T_at_steps_2 = [T_mix_ph(p, h, i2.fluid_data, i2.mixing_rule) for p, h in zip(p_at_steps_2, h_at_steps_2)]
+
+        # counter flow version
+        td_at_steps = [T1 - T2 for T1, T2 in zip(T_at_steps_1, T_at_steps_2[::-1])]
+        # parallel flow version
+        # td_at_steps = [T1 - T2 for T1, T2 in zip(T_at_steps_1, T_at_steps_2)]
+
+        td_log_in_sections = [
+            (td_at_steps[i + 1] - td_at_steps[i])
+            / np.log(td_at_steps[i + 1] / td_at_steps[i])
+            for i in range(sections)
+        ]
+        print(Q_in_sections)
+        print(td_log_in_sections)
+        UA_in_sections = [abs(Q) / td_log for Q, td_log in zip(Q_in_sections, td_log_in_sections)]
+        print(UA_in_sections)
+        return UA_in_sections
+
+    def UA_func(self, **kwargs):
+        r"""
+        Calculate heat transfer from heat transfer coefficients for
+        desuperheating and condensation as well as total heat exchange area.
+
+        Returns
+        -------
+        residual : float
+            Residual value of equation.
+        """
+        return self.UA.val - sum(self.calc_UA_in_sections())
+
+    def UA_deriv(self, increment_filter, k):
+        r"""
+        Partial derivatives of heat transfer coefficient function.
+
+        Parameters
+        ----------
+        increment_filter : ndarray
+            Matrix for filtering non-changing variables.
+
+        k : int
+            Position of derivatives in Jacobian matrix (k-th equation).
+        """
+        f = self.UA_func
+        for c in self.inl + self.outl:
+            if self.is_variable(c.m):
+                self.jacobian[k, c.m.J_col] = self.numeric_deriv(f, "m", c)
+            if self.is_variable(c.p):
+                self.jacobian[k, c.p.J_col] = self.numeric_deriv(f, 'p', c)
+            if self.is_variable(c.h):
+                self.jacobian[k, c.h.J_col] = self.numeric_deriv(f, 'h', c)
+
+    def td_pinch_func(self):
+        r"""
+        Equation for pinch point temperature difference of a condenser.
+
+        Returns
+        -------
+        residual : float
+            Residual value of equation.
+
+            .. math::
+
+                0 = td_\text{pinch} - T_\text{sat,in,1}
+                + T_\left(
+                    p_\text{in,2},\left[
+                        h_\text{in,2}
+                        + \frac{\dot Q_\text{cond}}{\dot m_\text{in,2}}
+                    \right]
+                \right)
+        """
+        o1 = self.outl[0]
+        i2 = self.inl[1]
+
+        h_sat = h_mix_pQ(o1.p.val_SI, 1, o1.fluid_data)
+
+        if o1.h.val_SI < h_sat:
+            # we have two sections in this case
+            Q_cond = o1.m.val_SI * (o1.h.val_SI - h_sat)
+
+            # calculate the intermediate temperatures
+            T_cond_i1 =  o1.calc_T_sat()
+            h_cond_o2 = i2.h.val_SI + abs(Q_cond) / i2.m.val_SI
+            T_cond_o2 = T_mix_ph(i2.p.val_SI, h_cond_o2, i2.fluid_data)
+
+            return self.td_pinch.val - T_cond_i1 + T_cond_o2
+
+        else:
+            o2 = self.outl[1]
+            return self.td_pinch.val - o1.calc_T_sat() + o2.calc_T()
+
+    def td_pinch_deriv(self, increment_filter, k):
+        """
+        Calculate partial derivates of upper terminal temperature function.
+
+        Parameters
+        ----------
+        increment_filter : ndarray
+            Matrix for filtering non-changing variables.
+
+        k : int
+            Position of derivatives in Jacobian matrix (k-th equation).
+        """
+        f = self.td_pinch_func
+        for c in [self.outl[0], self.inl[1]]:
+            if self.is_variable(c.m, increment_filter):
+                self.jacobian[k, c.m.J_col] = self.numeric_deriv(f, 'm', c)
+            if self.is_variable(c.p, increment_filter):
+                self.jacobian[k, c.p.J_col] = self.numeric_deriv(f, 'p', c)
+            if self.is_variable(c.h, increment_filter):
+                self.jacobian[k, c.h.J_col] = self.numeric_deriv(f, 'h', c)
+
+    def calc_parameters(self):
+        super().calc_parameters()
+        self.UA.val = sum(self.calc_UA_in_sections())
+
+
 class MovingBoundaryCondenser(HeatExchanger):
 
     @staticmethod

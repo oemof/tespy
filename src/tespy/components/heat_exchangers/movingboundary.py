Fix a bug that applied the wrong order of heat exchange rates for the different sections for cold side temperature calculation

src/tespy/components/heat_exchangers/movingboundary.py
@@ -10,12 +10,13 @@ tespy/components/heat_exchangers/movingboundary.py
 
 SPDX-License-Identifier: MIT
 """
-import numpy as np
+import math
 
 from tespy.components.heat_exchangers.base import HeatExchanger
 from tespy.tools.data_containers import ComponentProperties as dc_cp
 from tespy.tools.data_containers import GroupedComponentProperties as dc_gcp
 from tespy.tools.fluid_properties import T_mix_ph
+from tespy.tools.global_vars import ERR
 from tespy.tools.fluid_properties import h_mix_pQ
 
 
@@ -44,13 +45,13 @@ class MovingBoundaryHeatExchanger(HeatExchanger):
         h_sat_gas = h_mix_pQ(o1.p.val_SI, 1, o1.fluid_data)
         h_sat_liquid = h_mix_pQ(o1.p.val_SI, 0, o1.fluid_data)
 
-        if i1.h.val_SI > h_sat_gas and o1.h.val_SI < h_sat_liquid:
+        if i1.h.val_SI > h_sat_gas + ERR and o1.h.val_SI < h_sat_liquid - ERR:
             h_at_steps_1 = [i1.h.val_SI, h_sat_gas, h_sat_liquid, o1.h.val_SI]
             sections = 3
 
-        elif ((i1.h.val_SI > h_sat_gas) ^ (o1.h.val_SI < h_sat_liquid)):
+        elif ((i1.h.val_SI > h_sat_gas + ERR) ^ (o1.h.val_SI < h_sat_liquid - ERR)):
             sections = 2
-            if i1.h.val_SI > h_sat_gas:
+            if i1.h.val_SI > h_sat_gas + ERR:
                 h_at_steps_1 = [i1.h.val_SI, h_sat_gas, o1.h.val_SI]
             else:
                 h_at_steps_1 = [i1.h.val_SI, h_sat_liquid, o1.h.val_SI]
@@ -65,7 +66,7 @@ class MovingBoundaryHeatExchanger(HeatExchanger):
         Q_in_sections = [i1.m.val_SI * (h_at_steps_1[i + 1] - h_at_steps_1[i]) for i in range(sections)]
 
         h_at_steps_2 = [i2.h.val_SI]
-        for Q in Q_in_sections:
+        for Q in Q_in_sections[::-1]:
             h_at_steps_2.append(h_at_steps_2[-1] + abs(Q) / i2.m.val_SI)
 
         p_at_steps_2 = [i2.p.val_SI for _ in range(sections + 1)]
@@ -78,13 +79,11 @@ class MovingBoundaryHeatExchanger(HeatExchanger):
 
         td_log_in_sections = [
             (td_at_steps[i + 1] - td_at_steps[i])
-            / np.log(td_at_steps[i + 1] / td_at_steps[i])
+            / math.log(td_at_steps[i + 1] / td_at_steps[i])
             for i in range(sections)
         ]
-        print(Q_in_sections)
-        print(td_log_in_sections)
         UA_in_sections = [abs(Q) / td_log for Q, td_log in zip(Q_in_sections, td_log_in_sections)]
-        print(UA_in_sections)
+
         return UA_in_sections
 
     def UA_func(self, **kwargs):
@@ -120,6 +119,27 @@ class MovingBoundaryHeatExchanger(HeatExchanger):
             if self.is_variable(c.h):
                 self.jacobian[k, c.h.J_col] = self.numeric_deriv(f, 'h', c)
 
+    def calc_td_pinch(self):
+        o1 = self.outl[0]
+        i2 = self.inl[1]
+
+        h_sat = h_mix_pQ(o1.p.val_SI, 1, o1.fluid_data)
+
+        if o1.h.val_SI < h_sat:
+            # we have two sections in this case
+            Q_cond = o1.m.val_SI * (o1.h.val_SI - h_sat)
+
+            # calculate the intermediate temperatures
+            T_cond_i1 =  o1.calc_T_sat()
+            h_cond_o2 = i2.h.val_SI + abs(Q_cond) / i2.m.val_SI
+            T_cond_o2 = T_mix_ph(i2.p.val_SI, h_cond_o2, i2.fluid_data)
+
+            return T_cond_i1 - T_cond_o2
+
+        else:
+            o2 = self.outl[1]
+            return o1.calc_T_sat() - o2.calc_T()
+
     def td_pinch_func(self):
         r"""
         Equation for pinch point temperature difference of a condenser.
@@ -139,25 +159,7 @@ class MovingBoundaryHeatExchanger(HeatExchanger):
                     \right]
                 \right)
         """
-        o1 = self.outl[0]
-        i2 = self.inl[1]
-
-        h_sat = h_mix_pQ(o1.p.val_SI, 1, o1.fluid_data)
-
-        if o1.h.val_SI < h_sat:
-            # we have two sections in this case
-            Q_cond = o1.m.val_SI * (o1.h.val_SI - h_sat)
-
-            # calculate the intermediate temperatures
-            T_cond_i1 =  o1.calc_T_sat()
-            h_cond_o2 = i2.h.val_SI + abs(Q_cond) / i2.m.val_SI
-            T_cond_o2 = T_mix_ph(i2.p.val_SI, h_cond_o2, i2.fluid_data)
-
-            return self.td_pinch.val - T_cond_i1 + T_cond_o2
-
-        else:
-            o2 = self.outl[1]
-            return self.td_pinch.val - o1.calc_T_sat() + o2.calc_T()
+        return self.td_pinch.val - self.calc_td_pinch()
 
     def td_pinch_deriv(self, increment_filter, k):
         """
@@ -183,7 +185,7 @@ class MovingBoundaryHeatExchanger(HeatExchanger):
     def calc_parameters(self):
         super().calc_parameters()
         self.UA.val = sum(self.calc_UA_in_sections())
-
+        self.td_pinch.val = self.calc_td_pinch()
 
 class MovingBoundaryCondenser(HeatExchanger):
 
@@ -256,8 +258,8 @@ class MovingBoundaryCondenser(HeatExchanger):
             if ttd_cond_l < 0:
                 ttd_cond_l = abs(ttd_cond_l)
 
-            td_log_desup = (ttd_desup_l - ttd_desup_u) / np.log(ttd_desup_l / ttd_desup_u)
-            td_log_cond = (ttd_cond_l - ttd_cond_u) / np.log(ttd_cond_l / ttd_cond_u)
+            td_log_desup = (ttd_desup_l - ttd_desup_u) / math.log(ttd_desup_l / ttd_desup_u)
+            td_log_cond = (ttd_cond_l - ttd_cond_u) / math.log(ttd_cond_l / ttd_cond_u)
 
             residual = (
                 Q_total
@@ -381,8 +383,8 @@ class MovingBoundaryCondenser(HeatExchanger):
         ttd_cond_u = T_cond_i1 - T_cond_o2
         ttd_cond_l = T_cond_o1 - T_cond_i2
 
-        td_log_desup = (ttd_desup_l - ttd_desup_u) / np.log(ttd_desup_l / ttd_desup_u)
-        td_log_cond = (ttd_cond_l - ttd_cond_u) / np.log(ttd_cond_l / ttd_cond_u)
+        td_log_desup = (ttd_desup_l - ttd_desup_u) / math.log(ttd_desup_l / ttd_desup_u)
+        td_log_cond = (ttd_cond_l - ttd_cond_u) / math.log(ttd_cond_l / ttd_cond_u)
 
         self.td_pinch.val = T_desup_o1 - T_desup_i2
 
773f125ccc60ce0d3ad4d19311d28b52b6058026
Francesco Witte <github@witte.sh>
2024-08-04 23:43:33 +0200

    Test all features of the condenser

--- tests/test_components/test_partload_model_movingboundarycondenser.py
+++ tests/test_components/test_partload_model_movingboundarycondenser.py
@@ -38,7 +38,7 @@ c12.set_attr(x=0, T=80)
 c1.set_attr(fluid={"INCOMP::Water": 1}, m=10, T=70, p=1, h0=1e5)
 c2.set_attr(h0=1e5, T=80)
 
-cd.set_attr(U_desup=4000, U_cond=16000)
+cd.set_attr(U_desup=4000, U_cond=16000, U_subcool=5000)
 
 nw.solve("design")
 nw.save("design")
@@ -71,20 +71,22 @@ nw.print_results()
 # exit()
 # print(c2.T.val)
 
+
 Q = []
 T_cond = []
 m_refrig = []
 dT_pinch = []
-Q_cond = []
-
-for m in np.linspace(12, 4.55, 40):
+Q_in_sections = []
+for m in np.linspace(12, 5, 20):
     c1.set_attr(m=m)
     nw.solve("design")
     m_refrig += [c12.m.val]
     T_cond += [c12.T.val]
     Q += [abs(cd.Q.val)]
-    Q_cond += [cd.Q_cond]
     dT_pinch += [cd.td_pinch.val]
+    _, h_at_steps = cd.get_U_sections_and_h_steps()
+    _, Q_in_section = cd.calc_td_log_and_Q_in_sections(h_at_steps)
+    Q_in_sections += [Q_in_section]
 
 
 from matplotlib import pyplot as plt
@@ -98,8 +100,8 @@ ax[1].scatter(Q, T_cond)
 ax[1].set_ylabel("condensation temperature")
 ax[2].scatter(Q, dT_pinch)
 ax[2].set_ylabel("pinch temperature difference")
-ax[3].scatter(Q, [abs(qc / q) for qc, q in zip(Q_cond, Q)], label="cond")
-ax[3].scatter(Q, [abs((q + qc) / q) for qc, q in zip(Q_cond, Q)], label="desup")
+ax[3].scatter(Q, [abs(q[0] / Q) for q, Q in zip(Q_in_sections, Q)], label="desup")
+ax[3].scatter(Q, [abs(q[1] / Q) for q, Q in zip(Q_in_sections, Q)], label="cond")
 ax[3].legend()
 ax[3].set_ylabel("heat transfer shares of total heat transfer")
 
@@ -108,32 +110,51 @@ ax[3].set_xlabel("total heat transfer")
 [_.grid() for _ in ax]
 
 plt.tight_layout()
-fig.savefig("mb_partload_m_changing.png")
+fig.savefig("mb_U_sections_diff_m.png")
 
+plt.close()
 
-fig, ax = plt.subplots(1)
+c1.set_attr(m=10)
+nw.solve("design")
+nw.print_results()
 
-for i, m in enumerate(np.linspace(12, 5, 25)):
+cd.set_attr(A=None, UA=cd.UA.val)
+
+Q = []
+T_cond = []
+m_refrig = []
+dT_pinch = []
+Q_in_sections = []
+for m in np.linspace(12, 5, 20):
     c1.set_attr(m=m)
     nw.solve("design")
+    m_refrig += [c12.m.val]
+    T_cond += [c12.T.val]
+    Q += [abs(cd.Q.val)]
+    # Q_cond += [cd.Q_cond]
+    dT_pinch += [cd.td_pinch.val]
+    _, h_at_steps = cd.get_U_sections_and_h_steps()
+    _, Q_in_section = cd.calc_td_log_and_Q_in_sections(h_at_steps)
+    Q_in_sections += [Q_in_section]
 
-    if i % 5 ==0:
+print(Q_in_sections)
 
-        if cd.T_desup_i2 > cd.T_desup_o1:
-            print("PINCH VIOLATION")
+fig, ax = plt.subplots(4, sharex=True, figsize=(12, 8))
 
-        cd.Q.val = abs(cd.Q.val)
-        cd.Q_cond = abs(cd.Q_cond)
+ax[0].scatter(Q, m_refrig)
+ax[0].set_ylabel("refrigerant mass flow")
+ax[1].scatter(Q, T_cond)
+ax[1].set_ylabel("condensation temperature")
+ax[2].scatter(Q, dT_pinch)
+ax[2].set_ylabel("pinch temperature difference")
+ax[3].scatter(Q, [abs(q[0] / Q) for q, Q in zip(Q_in_sections, Q)], label="desup")
+ax[3].scatter(Q, [abs(q[1] / Q) for q, Q in zip(Q_in_sections, Q)], label="cond")
+ax[3].legend()
+ax[3].set_ylabel("heat transfer shares of total heat transfer")
 
-        _ = ax.plot([0, cd.Q_cond, cd.Q.val], [c12.T.val, cd.T_desup_o1 - 273.15, c11.T.val], label=f"Q={round(cd.Q.val)}")
-        ax.plot([0, cd.Q_cond, cd.Q.val], [c1.T.val, cd.T_desup_i2 - 273.15, c2.T.val], c=_[0].get_color())
+ax[3].set_xlabel("total heat transfer")
 
-
-ax.set_ylabel("temperature")
-ax.set_xlabel("heat transfer")
-ax.grid()
-ax.legend()
+[_.grid() for _ in ax]
 
 plt.tight_layout()
-
-fig.savefig("mb_QT.png")
+fig.savefig("mb_UA_diff_m.png")

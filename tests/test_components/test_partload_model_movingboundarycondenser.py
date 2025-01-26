07b986345845560f2782d15ac54f6299eca81f41
Francesco Witte <github@witte.sh>
2025-01-26 15:17:54 +0100

    Update temporary testscript

--- tests/test_components/test_partload_model_movingboundarycondenser.py
+++ tests/test_components/test_partload_model_movingboundarycondenser.py
@@ -31,18 +31,53 @@ nw.add_conns(c1, c2, c11, c12)
 cp.set_attr(eta_s=0.8)
 
 # c10.set_attr(T=20, x=1)
-c11.set_attr(fluid={"NH3": 1}, Td_bp=5, p=46)
-c12.set_attr(Td_bp=-5, p=46)
+c11.set_attr(fluid={"NH3": 1}, m=10, Td_bp=40, p=40)
+c12.set_attr(x=0, p=40)
 
-c1.set_attr(fluid={"Water": 1}, m=1, p=0.3, Td_bp=-5, h0=1e5)
-c2.set_attr(Td_bp=5, p=0.3)
+c1.set_attr(fluid={"Water": 1}, p=1, Td_bp=-5, h0=1e5)
+c2.set_attr(p=1, Td_bp=20)
+
 
 # cd.set_attr(U_desup=4000, U_cond=16000, U_subcool=5000)
 
 nw.set_attr(iterinfo=False)
 nw.solve("design")
 
-for Td_bp in np.linspace(5, 0.2):
+c11.set_attr(p=None)
+c12.set_attr(p=None)
+cd.set_attr(td_pinch=5, pr1=1)
+nw.solve("design")
+c11.set_attr(Td_bp=30.4)
+# c12.set_attr(Td_bp=-Td_bp)
+nw.solve("design")
+
+# Q_sections = cd._assign_sections()
+# T_steps_hot, T_steps_cold = cd._get_T_at_steps(Q_sections)
+# print(cd.UA.val, cd.td_pinch.val, Q_sections, T_steps_hot, T_steps_cold)
+# print(cd._calc_UA_in_sections(T_steps_hot, T_steps_cold, np.diff(Q_sections)))
+
+# c11.set_attr(Td_bp=30.5)
+# # c12.set_attr(Td_bp=-Td_bp)
+# nw.solve("design")
+
+# # Q_sections = cd._assign_sections()
+# # print(cd.UA.val, cd.td_pinch.val, Q_sections, cd._get_T_at_steps(Q_sections))
+
+# c11.set_attr(Td_bp=30.4)
+# # c12.set_attr(Td_bp=-Td_bp)
+# nw.solve("design")
+
+# Q_sections2 = cd._assign_sections()
+# T_steps_hot2, T_steps_cold2 = cd._get_T_at_steps(Q_sections2)
+# print(cd.UA.val, cd.td_pinch.val, Q_sections, T_steps_hot2, T_steps_cold2)
+# print(cd._calc_UA_in_sections(T_steps_hot2, T_steps_cold2, np.diff(Q_sections2)))
+
+# exit()
+
+for Td_bp in np.linspace(40, 2):
     c11.set_attr(Td_bp=Td_bp)
-    c12.set_attr(Td_bp=-Td_bp)
+    # c12.set_attr(Td_bp=-Td_bp)
     nw.solve("design")
+    # print(cd.td_pinch.val, cd.UA.val)
+    Q_sections = cd._assign_sections()
+    print(Td_bp, cd.UA.val)

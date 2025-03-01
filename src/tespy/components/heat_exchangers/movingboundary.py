eec171dee6719a497a94a9e27add240d08d7205c
Francesco Witte <github@witte.sh>
2025-03-01 13:04:50 +0100

    Throw a warning for non-equal inlet and outlet pressure and add an example with documentation

--- src/tespy/components/heat_exchangers/movingboundary.py
+++ src/tespy/components/heat_exchangers/movingboundary.py
@@ -19,9 +19,243 @@ from tespy.tools.fluid_properties import h_mix_pQ
 from tespy.tools.fluid_properties import single_fluid
 from tespy.tools.fluid_properties import T_mix_ph
 from tespy.tools.global_vars import ERR
+from tespy.tools.logger import logger
 
 
 class MovingBoundaryHeatExchanger(HeatExchanger):
+    r"""
+    Class for counter current heat exchanger with UA sections.
+
+    The heat exchanger is internally discretized into multiple sections, which
+    are defined by phase changes. The component assumes, that no pressure
+    losses occur.
+
+    **Mandatory Equations**
+
+    - :py:meth:`tespy.components.heat_exchangers.base.HeatExchanger.energy_balance_func`
+
+    **Optional Equations**
+
+    - :py:meth:`tespy.components.heat_exchangers.base.HeatExchanger.energy_balance_hot_func`
+    - :py:meth:`tespy.components.heat_exchangers.base.HeatExchanger.kA_func`
+    - :py:meth:`tespy.components.heat_exchangers.base.HeatExchanger.kA_char_func`
+    - :py:meth:`tespy.components.heat_exchangers.base.HeatExchanger.ttd_u_func`
+    - :py:meth:`tespy.components.heat_exchangers.base.HeatExchanger.ttd_l_func`
+    - :py:meth:`tespy.components.heat_exchangers.base.HeatExchanger.ttd_min_func`
+    - :py:meth:`tespy.components.heat_exchangers.base.HeatExchanger.eff_cold_func`
+    - :py:meth:`tespy.components.heat_exchangers.base.HeatExchanger.eff_hot_func`
+    - :py:meth:`tespy.components.heat_exchangers.base.HeatExchanger.eff_max_func`
+    - hot side :py:meth:`tespy.components.component.Component.pr_func`
+    - cold side :py:meth:`tespy.components.component.Component.pr_func`
+    - hot side :py:meth:`tespy.components.component.Component.zeta_func`
+    - cold side :py:meth:`tespy.components.component.Component.zeta_func`
+    - hot side :py:meth:`tespy.components.component.Component.dp_func`
+    - cold side :py:meth:`tespy.components.component.Component.dp_func`
+    - hot side :py:meth:`tespy.components.component.Component.UA_func`
+    - cold side :py:meth:`tespy.components.component.Component.td_pinch_func`
+
+    Inlets/Outlets
+
+    - in1, in2 (index 1: hot side, index 2: cold side)
+    - out1, out2 (index 1: hot side, index 2: cold side)
+
+    Image
+
+    .. image:: /api/_images/HeatExchanger.svg
+       :alt: flowsheet of the heat exchanger
+       :align: center
+       :class: only-light
+
+    .. image:: /api/_images/HeatExchanger_darkmode.svg
+       :alt: flowsheet of the heat exchanger
+       :align: center
+       :class: only-dark
+
+    Parameters
+    ----------
+    label : str
+        The label of the component.
+
+    design : list
+        List containing design parameters (stated as String).
+
+    offdesign : list
+        List containing offdesign parameters (stated as String).
+
+    design_path : str
+        Path to the components design case.
+
+    local_offdesign : boolean
+        Treat this component in offdesign mode in a design calculation.
+
+    local_design : boolean
+        Treat this component in design mode in an offdesign calculation.
+
+    char_warnings : boolean
+        Ignore warnings on default characteristics usage for this component.
+
+    printout : boolean
+        Include this component in the network's results printout.
+
+    Q : float, dict
+        Heat transfer, :math:`Q/\text{W}`.
+
+    pr1 : float, dict, :code:`"var"`
+        Outlet to inlet pressure ratio at hot side, :math:`pr/1`.
+
+    pr2 : float, dict, :code:`"var"`
+        Outlet to inlet pressure ratio at cold side, :math:`pr/1`.
+
+    dp1 : float, dict, :code:`"var"`
+        Inlet to outlet pressure delta at hot side, unit is the network's
+        pressure unit!.
+
+    dp2 : float, dict, :code:`"var"`
+        Inlet to outlet pressure delta at cold side, unit is the network's
+        pressure unit!.
+
+    zeta1 : float, dict, :code:`"var"`
+        Geometry independent friction coefficient at hot side,
+        :math:`\frac{\zeta}{D^4}/\frac{1}{\text{m}^4}`.
+
+    zeta2 : float, dict, :code:`"var"`
+        Geometry independent friction coefficient at cold side,
+        :math:`\frac{\zeta}{D^4}/\frac{1}{\text{m}^4}`.
+
+    ttd_l : float, dict
+        Lower terminal temperature difference :math:`ttd_\mathrm{l}/\text{K}`.
+
+    ttd_u : float, dict
+        Upper terminal temperature difference :math:`ttd_\mathrm{u}/\text{K}`.
+
+    ttd_min : float, dict
+        Minumum terminal temperature difference :math:`ttd_\mathrm{min}/\text{K}`.
+
+    eff_cold : float, dict
+        Cold side heat exchanger effectiveness :math:`eff_\text{cold}/\text{1}`.
+
+    eff_hot : float, dict
+        Hot side heat exchanger effectiveness :math:`eff_\text{hot}/\text{1}`.
+
+    eff_max : float, dict
+        Max value of hot and cold side heat exchanger effectiveness values
+        :math:`eff_\text{max}/\text{1}`.
+
+    kA : float, dict
+        Area independent heat transfer coefficient,
+        :math:`kA/\frac{\text{W}}{\text{K}}`.
+
+    kA_char : dict
+        Area independent heat transfer coefficient characteristic.
+
+    kA_char1 : tespy.tools.characteristics.CharLine, dict
+        Characteristic line for hot side heat transfer coefficient.
+
+    kA_char2 : tespy.tools.characteristics.CharLine, dict
+        Characteristic line for cold side heat transfer coefficient.
+
+    UA : float, dict
+        Sum of UA in all sections of the heat exchanger.
+
+    td_pinch : float, dict
+        Value of the lowest delta T between hot side and cold side at the
+        different sections.
+
+    Note
+    ----
+    The equations only apply to counter-current heat exchangers.
+
+    Example
+    -------
+    Water vapor should be cooled down, condensed and then further subcooled. For
+    this air is heated up from 15 °C to 25 °C.
+
+    >>> from tespy.components import Source, Sink, MovingBoundaryHeatExchanger
+    >>> from tespy.connections import Connection
+    >>> from tespy.networks import Network
+    >>> import numpy as np
+    >>> nw = Network(T_unit="C", p_unit="bar")
+    >>> nw.set_attr(iterinfo=False)
+    >>> so1 = Source("vapor source")
+    >>> so2 = Source("air source")
+    >>> cd = MovingBoundaryHeatExchanger("condenser")
+    >>> si1 = Sink("water sink")
+    >>> si2 = Sink("air sink")
+    >>> c1 = Connection(so1, "out1", cd, "in1", label="1")
+    >>> c2 = Connection(cd, "out1", si1, "in1", label="2")
+    >>> c11 = Connection(so2, "out1", cd, "in2", label="11")
+    >>> c12 = Connection(cd, "out2", si2, "in1", label="12")
+    >>> nw.add_conns(c1, c2, c11, c12)
+
+    To generate good guess values, first we run the simulation with fixed
+    pressure on the water side. The water enters at superheated vapor state
+    with 15 °C superheating and leaves it with 10 °C subcooling.
+
+    >>> c1.set_attr(fluid={"Water": 1}, p=1, Td_bp=15, m=1)
+    >>> c2.set_attr(p=1, Td_bp=-15)
+    >>> c11.set_attr(fluid={"Air": 1}, p=1, T=15)
+    >>> c12.set_attr(p=1, T=25)
+    >>> nw.solve("design")
+
+    Now we can remove the pressure specifications on the air side and impose
+    the minimum pinch instead, which will determine the actual water
+    condensation pressure.
+
+    >>> c1.set_attr(p=None)
+    >>> c2.set_attr(p=None)
+    >>> c12.set_attr(p=None)
+    >>> cd.set_attr(td_pinch=5, pr1=1, pr2=1)
+    >>> nw.solve("design")
+    >>> round(c1.p.val, 3)
+    0.056
+
+    We can also see the temperature differences in all sections of the heat
+    exchanger. Since the water vapor is cooled, condensed and then subcooled,
+    while the air does not change phase, three sections will form:
+
+    >>> Q_sections = cd._assign_sections()
+    >>> T_steps_hot, T_steps_cold = cd._get_T_at_steps(Q_sections)
+    >>> delta_T_between_sections = [
+    ...     round(T_hot - T_cold, 2)
+    ...     for T_hot, T_cold in zip(T_steps_hot, T_steps_cold)
+    ... ]
+    >>> delta_T_between_sections
+    [5.0, 19.75, 10.11, 25.0]
+
+    We can see that the lowest delta T is the first one. This is the delta T
+    between the hot side outlet and the cold side inlet, which can also be seen
+    if we have a look at the network's results.
+
+    >>> ();nw.print_results();() # doctest: +ELLIPSIS
+    (...)
+
+    If we change the subcooling degree at the water outlet, the condensation
+    pressure and pinch will move.
+
+    >>> c2.set_attr(Td_bp=-5)
+    >>> nw.solve("design")
+    >>> round(c1.p.val, 3)
+    0.042
+    >>> Q_sections = cd._assign_sections()
+    >>> T_steps_hot, T_steps_cold = cd._get_T_at_steps(Q_sections)
+    >>> delta_T_between_sections = [
+    ...     round(T_hot - T_cold, 2)
+    ...     for T_hot, T_cold in zip(T_steps_hot, T_steps_cold)
+    ... ]
+    >>> delta_T_between_sections
+    [9.88, 14.8, 5.0, 19.88]
+
+    Finally, in contrast to the baseclass :code:`HeatExchanger` `kA` value, the
+    `UA` value takes into account the heat transfer per section and calculates
+    the heat transfer coefficient as the sum of all sections, while the `kA`
+    value only takes into account the inlet and outlet temperatures and the
+    total heat transfer.
+
+    >>> round(cd.kA.val)
+    173307
+    >>> round(cd.UA.val)
+    273449
+    """
 
     @staticmethod
     def component():
@@ -330,3 +564,19 @@ class MovingBoundaryHeatExchanger(HeatExchanger):
         super().calc_parameters()
         self.UA.val = self.calc_UA()
         self.td_pinch.val = self.calc_td_pinch()
+        if round(self.inl[0].p.val_SI) != round(self.outl[0].p.val_SI):
+            msg = (
+                f"The {self.__class__.__name__} instance {self.label} is "
+                "defined for constant pressure. The identification of the "
+                "heat transfer sections might be wrong in case phase changes "
+                "are involved in the heat transfer process."
+            )
+            logger.warning(msg)
+        if round(self.inl[1].p.val_SI) != round(self.outl[1].p.val_SI):
+            msg = (
+                f"The {self.__class__.__name__} instance {self.label} is "
+                "defined for constant pressure. The identification of the "
+                "heat transfer sections might be wrong in case phase changes "
+                "are involved in the heat transfer process."
+            )
+            logger.warning(msg)

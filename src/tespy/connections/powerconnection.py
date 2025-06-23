247e42532e32a194a123ea1b04e9dd062b8b976b
Francesco Witte <github@witte.sh>
2025-06-23 21:25:18 +0200

    Add a draft for a powerconnection

---
+++ src/tespy/connections/powerconnection.py
@@ -0,0 +1,181 @@
+import numpy as np
+
+from tespy.components.component import Component
+from tespy.components.component import component_registry
+from tespy.connections import Connection
+from tespy.connections.connection import _ConnectionBase
+from tespy.connections.connection import connection_registry
+from tespy.tools.data_containers import FluidProperties as dc_prop
+from tespy.tools.helpers import TESPyConnectionError
+from tespy.tools.helpers import _get_dependents
+from tespy.tools.helpers import _get_vector_dependents
+from tespy.tools.helpers import _partial_derivative
+from tespy.tools.helpers import _partial_derivative_vecvar
+from tespy.tools.logger import logger
+
+
+@connection_registry
+class PowerConnection(_ConnectionBase):
+
+    def __init__(self, source, outlet_id, target, inlet_id, label=None, **kwargs):
+        self._check_types(source, target)
+        self._check_self_connect(source, target)
+        self._check_connector_id(source, outlet_id, source.poweroutlets())
+        self._check_connector_id(target, inlet_id, target.powerinlets())
+
+        self.label = f"{source.label}:{outlet_id}_{target.label}:{inlet_id}"
+        if label is not None:
+            self.label = label
+            if not isinstance(label, str):
+                msg = "Please provide the label as string."
+                logger.error(msg)
+                raise TypeError(msg)
+
+        # set specified values
+        self.source = source
+        self.source_id = outlet_id
+        self.target = target
+        self.target_id = inlet_id
+
+        # defaults
+        self.new_design = True
+        self.design_path = None
+        self.design = []
+        self.offdesign = []
+        self.local_design = False
+        self.local_offdesign = False
+        self.printout = True
+
+        # set default values for kwargs
+        self.property_data = self.get_parameters()
+        self.parameters = {
+            k: v for k, v in self.get_parameters().items()
+            if hasattr(v, "func") and v.func is not None
+        }
+        self.__dict__.update(self.property_data)
+        msg = (
+            f"Created connection from {self.source.label} ({self.source_id}) "
+            f"to {self.target.label} ({self.target_id})."
+        )
+        logger.debug(msg)
+        self.set_attr(**kwargs)
+
+    def set_attr(self, **kwargs):
+        r"""
+        Set, reset or unset attributes of a connection.
+
+        Parameters
+        ----------
+        e : float
+            Energy flow specification.
+
+        design : list
+            List containing design parameters (stated as string).
+
+        offdesign : list
+            List containing offdesign parameters (stated as string).
+
+        design_path : str
+            Path to individual design case for this connection.
+
+        local_offdesign : boolean
+            Treat this connection in offdesign mode in a design calculation.
+
+        local_design : boolean
+            Treat this connection in design mode in an offdesign calculation.
+
+        printout : boolean
+            Include this connection in the network's results printout.
+        """
+        # set specified values
+        for key in kwargs:
+            if key == 'label':
+                msg = 'Label can only be specified on instance creation.'
+                logger.error(msg)
+                raise TESPyConnectionError(msg)
+
+            elif key in self.property_data or key in self.property_data0:
+                self._parameter_specification(key, kwargs[key])
+
+            # design/offdesign parameter list
+            elif key in ['design', 'offdesign']:
+                if not isinstance(kwargs[key], list):
+                    msg = f"Please provide the {key} parameters as list!"
+                    logger.error(msg)
+                    raise TypeError(msg)
+                elif set(kwargs[key]).issubset(self.property_data.keys()):
+                    self.__dict__.update({key: kwargs[key]})
+                else:
+                    params = ', '.join(self.property_data.keys())
+                    msg = (
+                        "Available parameters for (off-)design specification "
+                        f"are: {params}."
+                    )
+                    logger.error(msg)
+                    raise ValueError(msg)
+
+            # design path
+            elif key == 'design_path':
+                self.__dict__.update({key: kwargs[key]})
+                self.new_design = True
+
+            # other boolean keywords
+            elif key in ['printout', 'local_design', 'local_offdesign']:
+                if not isinstance(kwargs[key], bool):
+                    msg = ('Please provide the ' + key + ' as boolean.')
+                    logger.error(msg)
+                    raise TypeError(msg)
+                else:
+                    self.__dict__.update({key: kwargs[key]})
+
+            # invalid keyword
+            else:
+                msg = f"Connection has no attribute {key}."
+                logger.error(msg)
+                raise KeyError(msg)
+
+    def _precalc_guess_values(self):
+        pass
+
+    def _presolve(self):
+        return []
+
+
+    def _reset_design(self, redesign):
+        for value in self.get_variables().values():
+            value.design = np.nan
+
+        self.new_design = True
+
+        # switch connections to design mode
+        if redesign:
+            for var in self.design:
+                self.get_attr(var).is_set = True
+
+            for var in self.offdesign:
+                self.get_attr(var).is_set = False
+
+    def get_variables(self):
+        return {"e": self.e}
+
+    def get_parameters(self):
+        return {"e": dc_prop(d=1e-4)}
+
+    def calc_results(self):
+        self.e.val = self.e.val_SI
+        return True
+
+    @classmethod
+    def _print_attributes(cls):
+        return ["e"]
+
+    @classmethod
+    def _result_attributes(cls):
+        return ["e"]
+
+    @classmethod
+    def _get_result_cols(cls, all_fluids):
+        return ["e"]
+
+    def collect_results(self, all_fluids):
+        return [self.e.val]

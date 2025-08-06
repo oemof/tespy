95eddea7cae6defb5f745a3df293f0c92665a476
Francesco Witte <github@witte.sh>
2025-08-06 12:46:26 +0200

    Add a couple of tests for the Node class

---
+++ tests/test_components/test_node.py
@@ -0,0 +1,148 @@
+from pytest import fixture
+from pytest import approx
+from numpy.testing import assert_almost_equal
+
+from tespy.networks import Network
+from tespy.components import Source, Sink, Node, Merge, Splitter
+from tespy.connections import Connection
+
+
+@fixture
+def node_network():
+    nw = Network()
+
+    so1 = Source("source1")
+    so2 = Source("source2")
+    si1 = Sink("sink1")
+    si2 = Sink("sink2")
+
+    node = Node("node")
+    node.set_attr(num_in=2, num_out=2)
+
+    c1 = Connection(so1, "out1", node, "in1", label="i1")
+    c2 = Connection(so2, "out1", node, "in2", label="i2")
+    c3 = Connection(node, "out1", si1, "in1", label="o1")
+    c4 = Connection(node, "out2", si2, "in1", label="o2")
+
+    nw.add_conns(c1, c2, c3, c4)
+
+    return nw
+
+
+@fixture
+def merge_splitter_network():
+    nw = Network()
+
+    so1 = Source("source1")
+    so2 = Source("source2")
+    si1 = Sink("sink1")
+    si2 = Sink("sink2")
+
+    merge = Merge("merge")
+    splitter = Splitter("splitter")
+    merge.set_attr(num_in=2)
+    splitter.set_attr(num_out=2)
+
+    c1 = Connection(so1, "out1", merge, "in1", label="i1")
+    c2 = Connection(so2, "out1", merge, "in2", label="i2")
+    c0 = Connection(merge, "out1", splitter, "in1", label="c1")
+    c3 = Connection(splitter, "out1", si1, "in1", label="o1")
+    c4 = Connection(splitter, "out2", si2, "in1", label="o2")
+
+    nw.add_conns(c1, c2, c0, c3, c4)
+
+    return nw
+
+
+def test_node_equals_merge_splitter_model(node_network, merge_splitter_network):
+
+    c1, c2, c3 = node_network.get_conn(["i1", "i2", "o1"])
+    c1.set_attr(fluid={"O2": 1}, m=5, p=1e5, T=300)
+    c2.set_attr(fluid={"N2": 1}, m=5, T=350)
+    c3.set_attr(m=2)
+
+    node_network.solve("design")
+
+    c1, c2, c3 = merge_splitter_network.get_conn(["i1", "i2", "o1"])
+    c1.set_attr(fluid={"O2": 1}, m=5, p=1e5, T=300)
+    c2.set_attr(fluid={"N2": 1}, m=5, T=350)
+    c3.set_attr(m=2)
+
+    merge_splitter_network.solve("design")
+
+    # the node network has the index subset
+    index = node_network.results["Connection"].index
+    number_cols = node_network.results["Connection"].dtypes == float
+
+    assert_almost_equal(
+        node_network.results["Connection"].loc[index, number_cols].values,
+        merge_splitter_network.results["Connection"].loc[index, number_cols].values
+    )
+
+
+def test_node_with_set_outflow_composition(node_network):
+    c1, c2, c3, c4 = node_network.get_conn(["i1", "i2", "o1", "o2"])
+
+    c1.set_attr(fluid={"O2": 1}, m=5, p=1e5, T=300)
+    c2.set_attr(fluid={"N2": 1}, T=350)
+    c3.set_attr(m=3)
+    c4.set_attr(fluid={"O2": 0.4, "N2": 0.6})
+
+    node_network.solve("design")
+
+    assert approx(c3.m.val_SI + c4.m.val_SI) == c1.m.val_SI + c2.m.val_SI
+    assert approx(c3.fluid.val["O2"]) == c4.fluid.val["O2"]
+    assert (
+        approx((c3.m.val_SI  + c4.m.val_SI) * c4.fluid.val["O2"])
+        == c1.fluid.val["O2"] * c1.m.val_SI
+    )
+    assert approx(c3.h.val_SI) == c4.h.val_SI
+    assert (
+        approx(c3.h.val_SI * (c1.m.val_SI + c2.m.val_SI))
+        == (c1.h.val_SI * c1.m.val_SI + c2.h.val_SI * c2.m.val_SI)
+    )
+
+def test_node_with_set_outflow_composition_and_temperature(node_network):
+    c1, c2, c3, c4 = node_network.get_conn(["i1", "i2", "o1", "o2"])
+
+    c1.set_attr(fluid={"O2": 1}, m=5, p=1e5, T=300)
+    c2.set_attr(fluid={"N2": 1})
+    c3.set_attr(m=3)
+    c4.set_attr(fluid={"O2": 0.4, "N2": 0.6}, T=340)
+
+    node_network.solve("design")
+
+    assert approx(c3.m.val_SI + c4.m.val_SI) == c1.m.val_SI + c2.m.val_SI
+    assert approx(c3.fluid.val["O2"]) == c4.fluid.val["O2"]
+    assert (
+        approx((c3.m.val_SI  + c4.m.val_SI) * c4.fluid.val["O2"])
+        == c1.fluid.val["O2"] * c1.m.val_SI
+    )
+    assert approx(c3.h.val_SI) == c4.h.val_SI
+    assert (
+        approx(c3.h.val_SI * (c1.m.val_SI + c2.m.val_SI))
+        == (c1.h.val_SI * c1.m.val_SI + c2.h.val_SI * c2.m.val_SI)
+    )
+
+
+def test_node_with_set_pressure_at_outlet(node_network):
+    c1, c2, c3, c4 = node_network.get_conn(["i1", "i2", "o1", "o2"])
+
+    c1.set_attr(fluid={"O2": 1}, m=5, T=300)
+    c2.set_attr(fluid={"N2": 1})
+    c3.set_attr(m=3, p=1e5)
+    c4.set_attr(m=12, T=340)
+
+    node_network.solve("design")
+
+    assert approx(c3.m.val_SI + c4.m.val_SI) == c1.m.val_SI + c2.m.val_SI
+    assert approx(c3.fluid.val["O2"]) == c4.fluid.val["O2"]
+    assert (
+        approx((c3.m.val_SI  + c4.m.val_SI) * c4.fluid.val["O2"])
+        == c1.fluid.val["O2"] * c1.m.val_SI
+    )
+    assert approx(c3.h.val_SI) == c4.h.val_SI
+    assert (
+        approx(c3.h.val_SI * (c1.m.val_SI + c2.m.val_SI))
+        == (c1.h.val_SI * c1.m.val_SI + c2.h.val_SI * c2.m.val_SI)
+    )

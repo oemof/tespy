fbfac0ee282768bd8a7f72c5d65a94f99605d149
Francesco Witte <github@witte.sh>
2025-10-07 12:58:49 +0200

    Run isort

--- src/tespy/components/__init__.py
+++ src/tespy/components/__init__.py
@@ -13,8 +13,8 @@ from .heat_exchangers.desuperheater import Desuperheater  # noqa: F401
 from .heat_exchangers.movingboundary import MovingBoundaryHeatExchanger  # noqa: F401
 from .heat_exchangers.parabolic_trough import ParabolicTrough  # noqa: F401
 from .heat_exchangers.parallel import ParallelFlowHeatExchanger  # noqa: F401
-from .heat_exchangers.simple import SimpleHeatExchanger  # noqa: F401
 from .heat_exchangers.sectioned import SectionedHeatExchanger  # noqa: F401
+from .heat_exchangers.simple import SimpleHeatExchanger  # noqa: F401
 from .heat_exchangers.solar_collector import SolarCollector  # noqa: F401
 from .nodes.droplet_separator import DropletSeparator  # noqa: F401
 from .nodes.drum import Drum  # noqa: F401

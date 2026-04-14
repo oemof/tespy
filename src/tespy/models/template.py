0a126c1bdd974e9ee757b70dc4536e3e58a354b0
Konstantinos Papanikandros <konstantinos.papanikandros@dlr.de>
2026-04-14 13:46:22 +0200

    Add state diagram methods

--- src/tespy/models/template.py
+++ src/tespy/models/template.py
@@ -1,6 +1,10 @@
 from tespy.networks import Network
 from tespy.tools.helpers import merge_dicts
-
+from fluprodia import FluidPropertyDiagram
+from matplotlib import pyplot as plt
+import numpy as np
+from tespy.tools import get_plotting_data
+from tespy.tools.fluid_properties import single_fluid
 
 class ModelTemplate():
 
@@ -70,15 +74,103 @@ class ModelTemplate():
             self._solved = False
             self.nw.solve("design", init_only=True, init_path=self._stable_solution)
 
-    def plot_Ts_diagram_matplotlib(self, subcycle=None):
-        if subcycle is not None:
-            relevant_connection = self._subcycle_mapping[subcycle]
-        else:
-            pass
-        # if no subcycle is provided all of them are plotted in individual figures
+    def plot_Ts_diagram_matplotlib(self, subcycle=None, save_path=None):
 
-    def plot_logph_diagram_matplotlib(self, subcycle=None):
-        pass
+        connection_label = self._subcycle_mapping().get(subcycle)
+        if connection_label is None:
+            raise ValueError("subcycle is unknown")
+        fluid_name = single_fluid(self.nw.get_conn(connection_label).fluid_data)
+        
+        if fluid_name is None:
+            raise ValueError("Fluid is mixture")
+        
+        print("FLUID: ", fluid_name)
+        print("CONNECTION LABEL: ", connection_label)
+        
+        diagram = FluidPropertyDiagram(fluid_name)
+
+        diagram.set_unit_system(self.nw.units)
+        diagram.set_isolines_subcritical(-20, 200)
+        diagram.calc_isolines()
+
+        # fig, ax = plt.subplots(1, 2, figsize=(10, 6))
+
+        processes, points = get_plotting_data(self.nw, connection_label)
+        processes = {
+            key: diagram.calc_individual_isoline(**value)
+            for key, value in processes.items()
+            if value is not None
+        }
+
+        fig, ax = plt.subplots(figsize=(10, 6))
+
+        diagram.draw_isolines(
+            fig, ax, "Ts", 1250, 2500, -20, 150,
+            isoline_data={
+                "Q": {"values": np.array([0.0, 0.2, 0.4, 0.6, 0.8, 1.0])},
+            }
+        )
+
+        for label, values in processes.items():
+            _ = ax.plot(values["s"], values["T"], label=label, color="tab:red", zorder=10000)
+
+        for label, point in points.items():
+            _ = ax.scatter(point["s"], point["T"], label=label, color="tab:red", zorder=10000)
+
+        if save_path:
+            fig.savefig(f"{save_path}/ts_diagram.svg", bbox_inches="tight")
+        return fig, ax
+    
+
+    def plot_logph_diagram_matplotlib(self, subcycle=None, save_path=None):
+
+
+        connection_label = self._subcycle_mapping().get(subcycle)
+        if connection_label is None:
+            raise ValueError("subcycle is unknown")
+        fluid_name = single_fluid(self.nw.get_conn(connection_label).fluid_data)
+        
+        if fluid_name is None:
+            raise ValueError("Fluid is mixture")
+        
+        print("FLUID: ", fluid_name)
+        print("CONNECTION LABEL: ", connection_label)
+        
+        diagram = FluidPropertyDiagram(fluid_name)
+
+        diagram.set_unit_system(self.nw.units)
+        diagram.set_isolines_subcritical(-20, 200)
+        diagram.calc_isolines()
+
+        # fig, ax = plt.subplots(1, 2, figsize=(10, 6))
+
+        processes, points = get_plotting_data(self.nw, connection_label)
+        processes = {
+            key: diagram.calc_individual_isoline(**value)
+            for key, value in processes.items()
+            if value is not None
+        }
+
+        fig, ax = plt.subplots(figsize=(10, 6))
+
+        diagram.draw_isolines(
+            fig, ax, "logph", 250000, 750000, 1, 50,
+            isoline_data={
+                "s": {"values": np.array([])},
+                "Q": {"values": np.array([0.0, 0.2, 0.4, 0.6, 0.8, 1.0])},
+            }
+        )
+
+        for label, values in processes.items():
+            _ = ax.plot(values["h"], values["p"], label=label, color="tab:red", zorder=10000)
+
+        for label, point in points.items():
+            _ = ax.scatter(point["h"], point["p"], label=label, color="tab:red", zorder=10000)
+
+        if save_path:
+            fig.savefig(f"{save_path}/log_ph_diagram.svg", bbox_inches="tight")
+        return fig, ax
+
 
     def plot_Ts_diagram_plotly(self, subcycle=None):
         pass

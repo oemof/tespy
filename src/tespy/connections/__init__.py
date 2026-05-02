33c826d9dabe73b80827369d9e379c687bfe0c10
Francesco Witte <github@witte.sh>
2026-05-02 14:12:21 +0200

    Remove Bus class

--- src/tespy/connections/__init__.py
+++ src/tespy/connections/__init__.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8
 
-from .bus import Bus  # noqa: F401
 from .connection import Connection  # noqa: F401
 from .connection import Ref  # noqa: F401
 from .humidairconnection import HAConnection  # noqa: F401

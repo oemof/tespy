83fb1e6fe784bf0f7d0ef3fd38e51032de1c6dfd
Francesco Witte <github@witte.sh>
2026-05-25 17:40:13 +0200

    Bump version

--- src/tespy/__init__.py
+++ src/tespy/__init__.py
@@ -3,7 +3,7 @@ import importlib.resources
 import os
 
 __datapath__ = os.path.join(importlib.resources.files("tespy"), "data")
-__version__ = '0.10.0.dev0'
+__version__ = "0.10.0rc1 - Rankine's Renaissance"
 
 # tespy data and connections import
 from . import connections  # noqa: F401

c87eaa4f581e4c82d9e108f95b3e38b0e6da5ca7
Francesco Witte <github@witte.sh>
2026-06-30 16:54:51 +0200

    Resolve DEFAULT char over self and parent classes

--- src/tespy/components/component.py
+++ src/tespy/components/component.py
@@ -423,11 +423,13 @@ class Component:
                 if data.func is not None:
                     self.char_specifications[key] = data.is_set
                 if data.char_func is None:
-                    try:
-                        data.char_func = ldc(
-                            self.__class__.__name__, key, 'DEFAULT', CharLine
-                        )
-                    except KeyError:
+                    for cls in type(self).__mro__:
+                        try:
+                            data.char_func = ldc(cls.__name__, key, 'DEFAULT', CharLine)
+                            break
+                        except KeyError:
+                            continue
+                    else:
                         data.char_func = CharLine()
 
             # component characteristics
@@ -435,11 +437,13 @@ class Component:
                 if data.func is not None:
                     self.char_specifications[key] = data.is_set
                 if data.char_func is None:
-                    try:
-                        data.char_func = ldc(
-                            self.__class__.__name__, key, 'DEFAULT', CharMap
-                        )
-                    except KeyError:
+                    for cls in type(self).__mro__:
+                        try:
+                            data.char_func = ldc(cls.__name__, key, 'DEFAULT', CharMap)
+                            break
+                        except KeyError:
+                            continue
+                    else:
                         data.char_func = CharMap()
 
             # grouped component properties
